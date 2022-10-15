fdc283528989d27258d9a3f80d6de589592a4429
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-15 11:50:32 +0100

    We can now get the direction the rover is facing as long as it is North.

--- test/getting_position_and_direction_rover_test.rb
+++ test/getting_position_and_direction_rover_test.rb
@@ -4,15 +4,21 @@ class MarsRover
   def coordinates
     OpenStruct.new(x: 0, y: 0)
   end
+
+  def direction
+    'N'
+  end
 end
 
 class GettingCurrentPositionAndDirectionOfMarsRoverTest < MiniTest::Test
   def test_current_position_and_direction_of_north_facing_mars_rover
 
     coordinates = MarsRover.new.coordinates
+    direction = MarsRover.new.direction
 
     assert_equal(0, coordinates.x)
     assert_equal(0, coordinates.y)
+    assert_equal('N', direction)
   end
 
   def test_current_position_of_north_facing_mars_rover_anywhere
