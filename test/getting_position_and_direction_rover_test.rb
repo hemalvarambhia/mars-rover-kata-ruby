3262850c79daf5d24711af00e42183c69d16c8e8
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-15 13:24:00 +0100

    We can now record the current position of the rover anywhere on the x-y axis.

--- test/getting_position_and_direction_rover_test.rb
+++ test/getting_position_and_direction_rover_test.rb
@@ -1,12 +1,13 @@
 require 'minitest/autorun'
 require 'ostruct'
 class MarsRover
-  def initialize(x: 0)
+  def initialize(x: 0, y: 0)
     @x = x
+    @y = y
   end
 
   def coordinates
-    OpenStruct.new(x: @x, y: 0)
+    OpenStruct.new(x: @x, y: @y)
   end
 
   def direction
@@ -34,6 +35,17 @@ class GettingCurrentPositionAndDirectionOfMarsRoverTest < MiniTest::Test
 
     assert_equal(1, coordinates.x)
     assert_equal(0, coordinates.y)
+    assert_equal('N', direction)
+  end
+
+  def test_current_position_of_north_facing_mars_rover_anywhere_on_y_axis
+    mars_rover = MarsRover.new(y: 5)
+
+    coordinates = mars_rover.coordinates
+    direction = mars_rover.direction
+
+    assert_equal(0, coordinates.x)
+    assert_equal(5, coordinates.y)
     assert_equal('N', direction)
   end
 
