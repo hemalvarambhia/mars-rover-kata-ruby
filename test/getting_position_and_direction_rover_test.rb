36c65065e1e403f565be42268b10091e7861ebed
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-15 13:58:08 +0100

    Extracted expected coordinates to its own variable.

--- test/getting_position_and_direction_rover_test.rb
+++ test/getting_position_and_direction_rover_test.rb
@@ -45,8 +45,9 @@ class GettingCurrentPositionAndDirectionOfMarsRoverTest < MiniTest::Test
     coordinates = mars_rover.coordinates
     direction = mars_rover.direction
 
-    assert_equal(0, coordinates.x)
-    assert_equal(5, coordinates.y)
+    expected_coordinates = OpenStruct.new(x: 0, y: 5)
+    assert_equal(expected_coordinates.x, coordinates.x)
+    assert_equal(expected_coordinates.y, coordinates.y)
     assert_equal('N', direction)
   end
 
