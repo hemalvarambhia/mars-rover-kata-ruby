84b2b181cf0076c9a1b2eede5fc21ab178df8c32
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-15 09:29:28 +0100

    Begun a test list for first requirement - the current location and position of the Mars rover kata.

---
+++ test/getting_position_and_direction_rover_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+
+class GettingCurrentPositionAndDirectionOfMarsRoverTest < MiniTest::Test
+  def test_current_position_and_direction_of_north_facing_mars_rover
+    skip('Test list')
+  end
+
+  def test_current_position_of_north_facing_mars_rover_anywhere
+    skip('Test list')
+  end
+
+  def test_current_position_and_direction_of_east_facing_mars_rover
+    skip('Test list')
+  end
+
+  def test_current_position_and_direction_of_west_facing_mars_rover
+    skip('Test list')
+  end
+end
