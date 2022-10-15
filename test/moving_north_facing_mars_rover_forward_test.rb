48299a94db8917c97cd90a6d938b723a10c8b9dc
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-15 15:54:07 +0100

    We cannot move the rover anywhere.

--- test/moving_north_facing_mars_rover_forward_test.rb
+++ test/moving_north_facing_mars_rover_forward_test.rb
@@ -1,7 +1,45 @@
 require 'minitest/autorun'
 
+class Coordinates
+  attr_reader :x, :y
+  def initialize(x:, y:)
+    @x = x
+    @y = y
+  end
+
+  def ==(coordinates)
+    x == coordinates.x && y == coordinates.y
+  end
+end
+
+class MarsRover
+  attr_reader :direction
+
+  def initialize(starting_coordinates:, direction:)
+    @current_coordinates = starting_coordinates
+    @direction = direction
+  end
+
+
+  def execute(_)
+  end
+
+  def coordinates
+    @current_coordinates
+  end
+end
+
 class MovingNorthFacingMarsRoverForward < MiniTest::Test
-  def test_failing_hookup
-    assert_equal(4, 2 + 2)
+  def test_moving_nowhere
+    mars_rover =
+      MarsRover.new(
+        starting_coordinates: Coordinates.new(x: 0, y: 0),
+        direction: 'N'
+      )
+
+    mars_rover.execute('')
+
+    coordinates = mars_rover.coordinates
+    assert_equal(Coordinates.new(x: 0, y: 0), coordinates)
   end
 end
