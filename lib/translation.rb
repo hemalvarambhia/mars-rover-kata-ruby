833c22b655a02a09174852e03d6470d0721d1fa2
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-16 17:49:23 +0100

    Added a new state variable and reader method that will replace x.

--- lib/translation.rb
+++ lib/translation.rb
@@ -1,15 +1,16 @@
 class Translation
-  attr_reader :x, :y
+  attr_reader :x, :y, :delta_x
 
   def initialize(delta_x:, delta_y:)
     @x = delta_x
     @y = delta_y
+    @delta_x = delta_x
   end
 
   def +(translation)
     Translation.new(
-      delta_x: x + translation.x,
-      delta_y: y + translation.y      
+      delta_x: x + translation.delta_x,
+      delta_y: y + translation.y
     )
   end
 end
