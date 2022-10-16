88ff2af7e38b3063149a30577385f27d0353138d
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-16 17:48:02 +0100

    Removed an unused method.

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -8,11 +8,7 @@ class Coordinates
   def translate(translation)
     Coordinates.new(x: x + translation.x, y: y + translation.y)
   end
-
-  def +(delta)
-    Coordinates.new(x: x + delta.x, y: y + delta.y)
-  end
-
+  
   def ==(coordinates)
     x == coordinates.x && y == coordinates.y
   end
