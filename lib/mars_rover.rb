6942071dd94bc0a96119cd9d0f4db01a0855880d
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-17 23:44:30 +0100

    Reduced the scope of the method.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,7 +1,5 @@
 require_relative './translation'
 class MarsRover
-  attr_reader :direction
-
   def initialize(starting_coordinates:, direction:)
     @current_coordinates = starting_coordinates
     @direction = direction
@@ -20,11 +18,13 @@ class MarsRover
     )
   end
 
+  private
+
+  attr_reader :direction
+
   def coordinates
     @current_coordinates
   end
-
-  private
 
   def to_movements(command)
     if command.empty?
