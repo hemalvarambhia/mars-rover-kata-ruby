3e50af7c95bd58317247412577338b39e8a82c65
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-17 23:31:44 +0100

    Simplified the assertion.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -3,11 +3,16 @@ module LocationAssertion
   private
 
   def assert_at_point(expected_coordinates, expected_direction, mars_rover)
-    point = OpenStruct.new(
-              coordinates: mars_rover.coordinates,
-              direction: mars_rover.direction
-            )
-    assert_equal(expected_coordinates, point.coordinates)
-    assert_equal(expected_direction, point.direction)
+    point =
+      OpenStruct.new(
+        coordinates: mars_rover.coordinates,
+        direction: mars_rover.direction
+      )
+    expected_point =
+      OpenStruct.new(
+        coordinates: expected_coordinates,
+        direction: expected_direction
+      )
+    assert_equal(expected_point, point)
   end
 end
