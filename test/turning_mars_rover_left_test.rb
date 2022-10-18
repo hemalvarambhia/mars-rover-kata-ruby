8951dd57585d2644e07065d092b45a33c31b205f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-18 14:01:54 +0100

    Added a test list for turning the Mars rover left.

---
+++ test/turning_mars_rover_left_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+class TurningMarsRoverLeftTest < Minitest::Test
+  def test_turning_north_facing_mars_rover
+    skip('Test list')
+  end
+
+  def test_turning_east_facing_mars_rover
+    skip('Test list')
+  end
+
+  def test_turning_south_facing_mars_rover
+    skip('Test list')
+  end
+
+  def test_turning_west_facing_mars_rover
+    skip('Test list')
+  end
+end
