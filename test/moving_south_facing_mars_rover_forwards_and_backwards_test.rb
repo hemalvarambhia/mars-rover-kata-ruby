b844cec4b94bdcb1519877894702ed17e402a242
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-18 13:43:27 +0100

    Added a missing test.

--- test/moving_south_facing_mars_rover_forwards_and_backwards_test.rb
+++ test/moving_south_facing_mars_rover_forwards_and_backwards_test.rb
@@ -39,4 +39,17 @@ class MovingSouthFacingMarsRoverForwardsAndBackwardsTest < MiniTest::Test
 
     assert_at_point(Coordinates.new(x: 3, y: 1), 'S', mars_rover)
   end
+
+  def test_rover_moves_nowhere_for_any_unrecognised_command
+    mars_rover =
+      MarsRover.new(
+        starting_coordinates: Coordinates.new(x: 0, y: 0),
+        direction: 'S'
+      )
+
+    mars_rover.execute('e')
+
+    expected_location = Location.new(coordinates: Coordinates.new(x: 0, y: 0), direction: 'S')
+    assert_located_at(expected_location, mars_rover)
+  end
 end
