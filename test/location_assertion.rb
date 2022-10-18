21fea1972c230652654483d367889086504fe065
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-18 10:45:33 +0100

    Introduced the idea of a location, with a coordinate and direction and migrated the assertion.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -1,11 +1,12 @@
 require 'ostruct'
+require_relative '../lib/location'
 module LocationAssertion
   private
 
   def assert_at_point(expected_coordinates, expected_direction, mars_rover)
     position = mars_rover.position
     expected_position =
-      OpenStruct.new(
+      Location.new(
         coordinates: expected_coordinates,
         direction: expected_direction
       )
