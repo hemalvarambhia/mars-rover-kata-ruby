0f1e09372a2675cc6765ea0b3b82ad67a34170bb
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-18 11:10:19 +0100

    Parallel Change, deprecated assertion now uses the new assertion.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -3,13 +3,16 @@ require_relative '../lib/location'
 module LocationAssertion
   private
 
+  def assert_located_at(expected_location, mars_rover)
+    assert_equal(expected_location, mars_rover.position)
+  end
+
   def assert_at_point(expected_coordinates, expected_direction, mars_rover)
-    position = mars_rover.position
     expected_position =
       Location.new(
         coordinates: expected_coordinates,
         direction: expected_direction
       )
-    assert_equal(expected_position, position)
+    assert_located_at(expected_position, mars_rover)
   end
 end
