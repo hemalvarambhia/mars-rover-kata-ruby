91c8f065baff2fd841ffb889a6e6a01069cd54ca
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-19 10:37:50 +0100

    Added an integration test.

---
+++ test/mars_rover_making_a_rectangle_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require_relative './location_assertion'
+require_relative './mars_rover_on_cartesian_coordinates'
+class MarsRoverMakingARectangleTest < Minitest::Test
+  include LocationAssertion
+
+  def test_making_a_rectangle
+    mars_rover = MarsRover.new(starting_coordinates: Coordinates.new(x: -2, y: 1), direction: 'S')
+
+    mars_rover.execute('fflfffflfflffff')
+
+    expected_location = Location.new(coordinates: Coordinates.new(x: -2, y: 1), direction: 'W')
+    assert_located_at(expected_location, mars_rover)
+  end
+end
