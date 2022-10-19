585121083986fb2729ef1e6638f4299111aad2e1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-19 10:48:01 +0100

    Added some missing tests.

--- test/mars_rover_turning_right_test.rb
+++ test/mars_rover_turning_right_test.rb
@@ -15,4 +15,12 @@ class MarsRoverTurningRightTest < Minitest::Test
   def test_turning_west_facing_mars_rover
     skip('Test list')
   end
+
+  def test_turning_north_facing_rover_twice
+    skip('Test list')
+  end
+
+  def test_turning_south_facing_rover_twice
+    skip('Test list')
+  end
 end
