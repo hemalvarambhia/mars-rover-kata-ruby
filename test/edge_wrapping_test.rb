1a3d546e38db5471fd678c25d76e38addd644a16
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-19 17:55:04 +0100

    Added a test list for edge wrapping.

---
+++ test/edge_wrapping_test.rb
@@ -0,0 +1,19 @@
+require 'minitest/autorun'
+class EdgeWrappingTest < Minitest::Test
+  def test_x_coordinate_does_not_wrap_when_well_inside_grid
+    skip('Test list')
+  end
+
+  def test_x_coordinate_wraps_when_at_the_right_hand_edge
+    skip('Test list')
+  end
+
+  def test_x_coordinate_wraps_when_moving_forward_past_the_right_hand_edge
+    skip('Test list')
+  end
+
+  def test_x_coordinate_wraps_when_at_the_left_hand_edge
+    skip('Test list')
+  end
+end
+
