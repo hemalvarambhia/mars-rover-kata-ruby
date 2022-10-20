ddc1b17d4e6177d1e165a31a45e64a47131e2f06
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-20 18:18:01 +0100

    Made the ordering much clearer.

--- test/wrapping_horizontally_test.rb
+++ test/wrapping_horizontally_test.rb
@@ -31,6 +31,16 @@ class WrappingHorizontallyTest < Minitest::Test
     assert_equal(expected_coordinate.y, coordinate.y)
   end
 
+  def test_x_coordinate_starting_away_from_origin_and_wrapping_back_to_origin
+    starting_coordinate = WrappingLocation.new(x_range: (-4..4), x: 2, y: 0)
+    coordinate = starting_coordinate
+    7.times { coordinate = coordinate.move_forward }
+
+    expected_coordinate = WrappingLocation.new(x_range: (-4..4), x: 0, y: 0)
+    assert_equal(expected_coordinate.x, coordinate.x)
+    assert_equal(expected_coordinate.y, coordinate.y)
+  end
+
   def test_x_coordinate_wraps_when_at_the_left_hand_edge
     starting_coordinate = WrappingLocation.new(x_range: (-4..4), x: -4, y: 0)
 
@@ -47,16 +57,6 @@ class WrappingHorizontallyTest < Minitest::Test
     coordinate = starting_coordinate.move_backward.move_backward
 
     expected_coordinate = WrappingLocation.new(x_range: (-4..4), x: 3, y: 0)
-    assert_equal(expected_coordinate.x, coordinate.x)
-    assert_equal(expected_coordinate.y, coordinate.y)
-  end
-
-  def test_x_coordinate_starting_away_from_origin_and_wrapping_back_to_origin
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), x: 2, y: 0)
-    coordinate = starting_coordinate
-    7.times { coordinate = coordinate.move_forward }
-
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), x: 0, y: 0)
     assert_equal(expected_coordinate.x, coordinate.x)
     assert_equal(expected_coordinate.y, coordinate.y)
   end
