9eb23ae168a56a75ba70d786a6b7b00efecbb090
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-20 13:56:32 +0100

    We can wrap when the coordinate is one step away from the right hand edge, and translation takes it past the edge.

--- test/edge_wrapping_when_translating_eastward_test.rb
+++ test/edge_wrapping_when_translating_eastward_test.rb
@@ -21,8 +21,17 @@ class EdgeWrappingWhenTranslatingEastwardTest < Minitest::Test
     assert_equal(expected_coordinate.y, coordinate.y)
   end
 
-  def test_x_coordinate_wraps_when_past_the_right_hand_edge
-    skip('Test list')
+  def test_x_coordinate_wraps_when_translates_past_the_right_hand_edge
+    starting_coordinate = WrappingCoordinates.new(x: 3, y: 0)
+
+    coordinate =
+      starting_coordinate
+        .translate(OpenStruct.new(delta_x: 1, delta_y: 0))
+        .translate(OpenStruct.new(delta_x: 1, delta_y: 0))
+
+    expected_coordinate = WrappingCoordinates.new(x: -4, y: 0)
+    assert_equal(expected_coordinate.x, coordinate.x)
+    assert_equal(expected_coordinate.y, coordinate.y)
   end
 
   def test_x_coordinate_wraps_when_at_the_left_hand_edge
