b2b00af05423ef84733b2bee168c12e803f796c2
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-20 19:04:45 +0100

    Added to the test list.

--- test/wrapping_location_test.rb
+++ test/wrapping_location_test.rb
@@ -11,4 +11,16 @@ class WrappingLocationTest < Minitest::Test
   def test_equals_is_transitive
     skip('Test list')
   end
+
+  def test_that_two_locations_with_different_x_coordinates_are_unequal
+    skip('Test list')
+  end
+
+  def test_that_two_locations_with_different_y_coordinates_are_unequal
+    skip('Test list')
+  end
+
+  def test_that_a_location_is_not_equal_to_an_object_of_a_different_type
+    skip('Test list')
+  end
 end
