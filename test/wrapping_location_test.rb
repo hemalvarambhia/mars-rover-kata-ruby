89f2e546334d0c1570fa426ab5c767888ff06331
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-20 18:58:08 +0100

    Added a test list for equals.

---
+++ test/wrapping_location_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+class WrappingLocationTest < Minitest::Test
+  def test_equals_is_reflexive
+    skip('Test list')
+  end
+
+  def test_equals_is_symmetric
+    skip('Test list')
+  end
+
+  def test_equals_is_transitive
+    skip('Test list')
+  end
+end
