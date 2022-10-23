a7d8145d9267068b073beaa3fcd6bbe845365f2f
hemalvarambhia <h.n.varambhia@gmail.com>
2022-10-23 19:22:25 +0100

    We now provide the world to the WrappingLocation.

--- lib/wrapping_location.rb
+++ lib/wrapping_location.rb
@@ -10,19 +10,19 @@ class WrappingLocation
 
   def move_forward
     if at_right_edge? && direction == 'E'
-      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, x: world.left_edge, y: y)
+      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, world: world, x: world.left_edge, y: y)
     elsif at_top_edge? && direction == 'N'
-       WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, x: 3, y: -4)
+       WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, world: world, x: 3, y: -4)
     else
-      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, x: x + 1, y: y)
+      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, world: world, x: x + 1, y: y)
     end
   end
 
   def move_backward
     if at_left_edge?
-      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, x: world.right_edge, y: y)
+      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, world: world, x: world.right_edge, y: y)
     else
-      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, x: x - 1, y: y)
+      WrappingLocation.new(x_range: world.x_range, y_range: world.y_range, world: world, x: x - 1, y: y)
     end
   end
 
