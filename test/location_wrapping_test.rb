5550ca730d748e9b32484a2793e586c996598d23
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-25 07:49:17 -0400

    Clients no longer provide the range.

--- test/location_wrapping_test.rb
+++ test/location_wrapping_test.rb
@@ -3,72 +3,72 @@ require_relative '../lib/wrapping_location'
 class LocationWrappingTest < Minitest::Test
   def test_inside_grid_and_no_possibility_of_wrapping
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 0, y: 0)
+    starting_coordinate = WrappingLocation.new(world: world, x: 0, y: 0)
 
     coordinate = starting_coordinate.move_forward
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 1, y: 0)
+    expected_coordinate = WrappingLocation.new(world: world, x: 1, y: 0)
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
   def test_wrapping_when_at_the_right_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 4, y: 0)
+    starting_coordinate = WrappingLocation.new(world: world, x: 4, y: 0)
 
     coordinate = starting_coordinate.move_forward
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: -4, y: 0)
+    expected_coordinate = WrappingLocation.new(world: world, x: -4, y: 0)
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
   def test_wrapping_past_the_right_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 3, y: 0)
+    starting_coordinate = WrappingLocation.new(world: world, x: 3, y: 0)
 
     coordinate = starting_coordinate.move_forward.move_forward
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: -4, y: 0)
+    expected_coordinate = WrappingLocation.new(world: world, x: -4, y: 0)
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
   def test_starting_away_from_origin_and_wrapping_back_to_origin
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 2, y: 0)
+    starting_coordinate = WrappingLocation.new(world: world, x: 2, y: 0)
     coordinate = starting_coordinate
 
     7.times { coordinate = coordinate.move_forward }
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 0, y: 0)
+    expected_coordinate = WrappingLocation.new(world: world, x: 0, y: 0)
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
   def test_x_wrapping_when_at_the_left_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: -4, y: 0)
+    starting_coordinate = WrappingLocation.new(world: world, x: -4, y: 0)
 
     coordinate = starting_coordinate.move_backward
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 4, y: 0)
+    expected_coordinate = WrappingLocation.new(world: world, x: 4, y: 0)
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
   def test_wrapping_past_the_left_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: -4, y: 0)
+    starting_coordinate = WrappingLocation.new(world: world, x: -4, y: 0)
 
     coordinate = starting_coordinate.move_backward.move_backward
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 3, y: 0)
+    expected_coordinate = WrappingLocation.new(world: world, x: 3, y: 0)
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
   def test_wrapping_when_at_the_top_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 3, y: 4, direction: 'N')
+    starting_coordinate = WrappingLocation.new(world: world, x: 3, y: 4, direction: 'N')
 
     coordinate = starting_coordinate.move_forward
 
-    expected_coordinate = WrappingLocation.new(x_range: (-4..4), y_range: (-4..4), world: world, x: 3, y: -4, direction: 'N')
+    expected_coordinate = WrappingLocation.new(world: world, x: 3, y: -4, direction: 'N')
     assert_coordinates_equal(coordinate, expected_coordinate)
   end
 
