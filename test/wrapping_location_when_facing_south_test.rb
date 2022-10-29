6e0fb18085371a7c896a559ef3b6f00f1a6d3573
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-29 19:19:04 +0100

    Added a test list for the next task: wrapping location when facing south.

---
+++ test/wrapping_location_when_facing_south_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require_relative '../lib/wrapping_location'
+class WrappingLocationWhenFacingSouthTest < Minitest::Test
+  def test_no_wrapping_when_moving_forward
+    skip('Test list')
+  end
+
+  def test_wrapping_when_at_bottom_edge_and_moving_forward
+    skip('Test list')
+  end
+
+  def test_wrapping_past_bottom_edge_when_moving_forward
+    skip('Test list')
+  end
+
+  def test_no_wrapping_when_moving_backward
+    skip('Test list')
+  end
+
+  def test_wrapping_when_at_top_edge_and_moving_backwards
+    skip('Test list')
+  end
+
+  def test_wrapping_past_top_edge_when_moving_forwards
+    skip('Test list')
+  end
+end
