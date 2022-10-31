8febd4174eec08e44d76ad85b383f98fb4f3ce7c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-31 19:17:38 +0000

    Added next test to get passing.

--- test/coordinate_assertion.rb
+++ test/coordinate_assertion.rb
@@ -4,10 +4,4 @@ module CoordinateAssertion
     assert_equal(expected_coordinates.y, coordinates.y, "y-coordinate")
     assert_equal(expected_coordinates.direction, coordinates.direction, "direction")
   end
-
-  def assert_coordinates_equal(coordinate, expected_coordinate)
-    assert_equal(expected_coordinate.x, coordinate.x, "x-coordinate")
-    assert_equal(expected_coordinate.y, coordinate.y, "y-coordinate")
-    assert_equal(expected_coordinate.direction, coordinate.direction, "direction")
-  end
 end
--- test/wrapping_location_when_facing_west_test.rb
+++ test/wrapping_location_when_facing_west_test.rb
@@ -1,8 +1,18 @@
 require 'minitest/autorun'
+require_relative './coordinate_assertion'
 require_relative '../lib/wrapping_location'
 class WrappingLocationWhenFacingWestTest < Minitest::Test
+  include CoordinateAssertion
+
   def test_no_wrapping_when_moving_forward
     skip('Test list')
+    world = World.new(x_range: (-3..3), y_range: (-3..3))
+    starting_coordinates = WrappingLocation.new(world: world, x: 3, y: 1, direction: 'W')
+
+    coordinates = starting_coordinates.move_forward
+
+    expected_coordinates = WrappingLocation.new(world: world, x: 2, y: 1, direction: 'W')
+    assert_equals(expected_coordinates, coordinates)
   end
 
   def test_no_wrapping_when_moving_forward_to_the_left_hand_edge
