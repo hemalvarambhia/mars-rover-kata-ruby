34e5eddc99840853300004b67fd96d5e272ac5a3
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-31 12:25:04 +0000

    Added a test list for wrapping location when facing west.

---
+++ test/wrapping_location_when_facing_west_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require_relative '../lib/wrapping_location'
+class WrappingLocationWhenFacingWestTest < Minitest::Test
+  def test_no_wrapping_when_moving_forward
+    skip('Test list')
+  end
+
+  def test_no_wrapping_when_moving_forward_to_the_left_hand_edge
+    skip('Test list')
+  end
+
+  def test_wrapping_when_at_the_left_hand_edge_and_moving_forward
+    skip('Test list')
+  end
+
+  def test_wrapping_past_the_left_hand_edge_and_moving_forward
+    skip('Test list')
+  end
+
+  def test_no_wrapping_when_moving_backward
+    skip('Test list')
+  end
+
+  def test_no_wrapping_when_moving_backward_to_the_right_hand_edge
+    skip('Test list')
+  end
+
+  def test_wrapping_when_at_the_right_hand_edge_and_moving_backward
+    skip('Test list')
+  end
+end
