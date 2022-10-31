02fee947e46ae903994d022e2e25ba7492f18c55
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-10-31 23:14:41 +0000

    Removed obsolete code.

--- lib/coordinates.rb
+++ lib/coordinates.rb
@@ -5,14 +5,6 @@ class Coordinates
     @y = y
   end
 
-  def translate(translation)
-    Coordinates.new(x: x + translation.delta_x, y: y + translation.delta_y)
-  end
-
-  def ==(coordinates)
-    x == coordinates.x && y == coordinates.y
-  end
-
   def inspect
     "(#{x}, #{y})"
   end
