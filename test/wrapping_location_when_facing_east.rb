4de7f89df4bcd36a10346c8a7aaf67686d5db4bc
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-01 12:07:54 +0000

    Migrated the test towards using the Mars Rover.

--- test/wrapping_location_when_facing_east.rb
+++ test/wrapping_location_when_facing_east.rb
@@ -90,11 +90,11 @@ class WrappingLocationWhenFacingEast < Minitest::Test
 
   def test_wrapping_past_the_left_hand_edge_when_moving_backward
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = Location.new(world: world, x: -4, y: 0, direction: 'E')
+    mars_rover = MarsRover.new(world: world, starting_coordinates: Coordinates.new(x: -4, y: 0), direction: 'E')
 
-    coordinate = starting_coordinate.move_backward.move_backward
+    mars_rover.execute('bb')
 
     expected_coordinate = Location.new(world: world, x: 3, y: 0, direction: 'E')
-    assert_equals(expected_coordinate, coordinate)
+    assert_located_at(expected_coordinate, mars_rover)
   end
 end
