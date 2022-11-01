75251cb9fddc9a7139051b27078091ad1febcf4e
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-01 17:27:40 +0000

    Migrated to using the Mars Rover to test location wrapping.

--- test/wrapping_location_when_facing_west_test.rb
+++ test/wrapping_location_when_facing_west_test.rb
@@ -20,12 +20,12 @@ class WrappingLocationWhenFacingWestTest < Minitest::Test
 
   def test_no_wrapping_when_moving_forward_to_the_left_hand_edge
     world = World.new(x_range: (-3..3), y_range: (-3..3))
-    starting_coordinates = Location.new(world: world, x: 2, y: -2, direction: 'W')
+    mars_rover = MarsRover.new(world: world, starting_coordinates: Coordinates.new(x: 2, y: -2), direction: 'W')
 
-    coordinates = starting_coordinates.move_forward
+    mars_rover.execute('f')
 
     expected_coordinates = Location.new(world: world, x: 1, y: -2, direction: 'W')
-    assert_equals(expected_coordinates, coordinates)
+    assert_located_at(expected_coordinates, mars_rover)
   end
 
   def test_wrapping_when_at_the_left_hand_edge_and_moving_forward
