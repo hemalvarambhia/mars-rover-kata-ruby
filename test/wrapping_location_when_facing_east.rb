ebb28c62bf0d10862db615dc9e898fe132d69208
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-01 11:38:58 +0000

    Migrated to testing via the Mars Rover.

--- test/wrapping_location_when_facing_east.rb
+++ test/wrapping_location_when_facing_east.rb
@@ -50,33 +50,32 @@ class WrappingLocationWhenFacingEast < Minitest::Test
 
   def test_starting_away_from_origin_and_wrapping_back_to_origin
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = Location.new(world: world, x: 2, y: 0, direction: 'E')
-    coordinate = starting_coordinate
+    mars_rover = MarsRover.new(world: world, starting_coordinates: Coordinates.new(x: 2, y: 0), direction: 'E')
 
-    7.times { coordinate = coordinate.move_forward }
+    mars_rover.execute('fffffff')
 
     expected_coordinate = Location.new(world: world, x: 0, y: 0, direction: 'E')
-    assert_equals(expected_coordinate, coordinate)
+    assert_located_at(expected_coordinate, mars_rover)
   end
 
   def test_no_wrapping_when_moving_backward
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = Location.new(world: world, x: 2, y: 0, direction: 'E')
+    mars_rover = MarsRover.new(world: world, starting_coordinates: Coordinates.new(x: 2, y: 0), direction: 'E')
 
-    coordinate = starting_coordinate.move_backward
+    mars_rover.execute('b')
 
     expected_coordinate = Location.new(world: world, x: 1, y: 0, direction: 'E')
-    assert_equals(expected_coordinate, coordinate)
+    assert_located_at(expected_coordinate, mars_rover)
   end
 
   def test_no_wrapping_when_moving_backward_to_the_left_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
-    starting_coordinate = Location.new(world: world, x: -3, y: 0, direction: 'E')
+    mars_rover = MarsRover.new(world: world, starting_coordinates: Coordinates.new(x: -3, y: 0), direction: 'E')
 
-    coordinate = starting_coordinate.move_backward
+    mars_rover.execute('b')
 
     expected_coordinate = Location.new(world: world, x: -4, y: 0, direction: 'E')
-    assert_equals(expected_coordinate, coordinate)
+    assert_located_at(expected_coordinate, mars_rover)
   end
 
   def test_wrapping_when_at_the_left_hand_edge_and_moving_backward
