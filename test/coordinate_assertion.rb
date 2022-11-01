a993af39bf514fea47df98c6f1f9b7ea281b003f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-01 18:02:44 +0000

    Removed obsolete code.

--- test/coordinate_assertion.rb
+++
@@ -1,7 +0,0 @@
-module CoordinateAssertion
-  def assert_equals(expected_coordinates, coordinates)
-    assert_equal(expected_coordinates.x, coordinates.x, "x-coordinate")
-    assert_equal(expected_coordinates.y, coordinates.y, "y-coordinate")
-    assert_equal(expected_coordinates.direction, coordinates.direction, "direction")
-  end
-end
