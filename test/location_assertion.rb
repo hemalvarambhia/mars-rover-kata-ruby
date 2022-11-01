03cc45cc5ee836f85d6508dfe70a8c8d9eb5e539
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-01 09:56:51 +0000

    Removed obsolete code.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -7,13 +7,4 @@ module LocationAssertion
     assert_equal(expected_location.y, coordinates.y)
     assert_equal(expected_location.direction, coordinates.direction)
   end
-
-  def assert_at_point(expected_coordinates, expected_direction, mars_rover)
-    expected_position =
-      Location.infinite(
-        coordinates: expected_coordinates,
-        direction: expected_direction
-      )
-    assert_located_at(expected_position, mars_rover)
-  end
 end
