b9fe5aa54f9be01b53cb7e39981b3d31944fec6e
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-02 13:24:43 +0000

    We have a location == method which assert_equals can use.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -3,8 +3,6 @@ module LocationAssertion
 
   def assert_located_at(expected_location, mars_rover)
     coordinates = mars_rover.position
-    assert_equal(expected_location.x, coordinates.x, "Current location: #{coordinates}")
-    assert_equal(expected_location.y, coordinates.y, "Current location: #{coordinates}")
-    assert_equal(expected_location.direction, coordinates.direction, "direction")
+    assert_equal(expected_location, coordinates, "Current location: #{coordinates}")
   end
 end
