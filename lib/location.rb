403b6128eccc14b4d3a82442fa1df1f4b9144d5c
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-05 20:44:50 +0000

    Moved logic back to Location.

--- lib/location.rb
+++ lib/location.rb
@@ -15,6 +15,35 @@ class Location
     x == location.x && y == location.y && direction == location.direction
   end
 
+  def move_forward
+    case direction
+    when 'E'
+      if world.at_right_edge?(self)
+        Location.new(world: world, x: world.left_edge, y: y, direction: direction)
+      else
+        Location.new(world: world, x: x + 1, y: y, direction: direction)
+      end
+    when 'N'
+      if world.at_top_edge?(self)
+        Location.new(world: world, x: x, y: world.bottom_edge, direction: direction)
+      else
+        Location.new(world: world, x: x, y: y + 1, direction: direction)
+      end
+    when 'S'
+      if world.at_bottom_edge?(self)
+        Location.new(world: world, x: x, y: world.top_edge, direction: direction)
+      else
+        Location.new(world: world, x: x, y: y - 1, direction: direction)
+      end
+    when 'W'
+      if world.at_left_edge?(self)
+        Location.new(world: world, x: world.right_edge, y: y, direction: direction)
+      else
+        Location.new(world: world, x: x - 1, y: y, direction: direction)
+      end
+    end
+  end
+
   def inspect
     "(#{x}, #{y}), facing #{direction}"
   end
