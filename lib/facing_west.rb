190f63a133aab33beff0d518c9d816f45d06313e
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-06 13:41:35 +0000

    Replaced references to the method argument with the state variable.

--- lib/facing_west.rb
+++ lib/facing_west.rb
@@ -4,22 +4,22 @@ class FacingWest
   end
 
   def move_forward(location)
-    if location.world.at_left_edge?(location)
-      Location.new(world: location.world, x: location.world.right_edge, y: location.y, direction: location.direction)
+    if @location.world.at_left_edge?(@location)
+      Location.new(world: @location.world, x: @location.world.right_edge, y: @location.y, direction: @location.direction)
     else
-      Location.new(world: location.world, x: location.x - 1, y: location.y, direction: location.direction)
+      Location.new(world: @location.world, x: @location.x - 1, y: @location.y, direction: @location.direction)
     end
   end
 
   def move_backward(location)
-    if location.world.at_right_edge?(location)
-      Location.new(world: location.world, x: location.world.left_edge, y: location.y, direction: location.direction)
+    if @location.world.at_right_edge?(@location)
+      Location.new(world: @location.world, x: @location.world.left_edge, y: @location.y, direction: @location.direction)
     else
-      Location.new(world: location.world, x: location.x + 1, y: location.y, direction: location.direction)
+      Location.new(world: @location.world, x: @location.x + 1, y: @location.y, direction: @location.direction)
     end
   end
 
   def turn_left(location)
-    Location.new(world: location.world, x: location.x, y: location.y, direction: 'S')
+    Location.new(world: @location.world, x: @location.x, y: @location.y, direction: 'S')
   end
 end
