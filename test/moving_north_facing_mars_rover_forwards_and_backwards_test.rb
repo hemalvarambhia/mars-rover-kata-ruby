9465b05d53d8544f0ffcbab38618d6c8a82c4eef
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-09 23:02:17 +0000

    Moved the tests to a more sensible place.

--- test/moving_north_facing_mars_rover_forwards_and_backwards_test.rb
+++ test/moving_north_facing_mars_rover_forwards_and_backwards_test.rb
@@ -100,4 +100,84 @@ class MovingNorthFacingMarsRoverForwardsAndBackwardsTest < MiniTest::Test
     expected_location = Location.new(world: World.infinite, x: 1, y: 0, direction: 'N')
     assert_located_at(expected_location, mars_rover)
   end
+
+  def test_no_wrapping_when_moving_forward
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 2, y: 1, direction: 'N'))
+
+    mars_rover.execute('f')
+
+    expected_coordinate = Location.new(world: world, x: 2, y: 2, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_moving_forward_to_the_top_edge
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 2, y: 3, direction: 'N'))
+
+    mars_rover.execute('f')
+
+    expected_coordinate = Location.new(world: world, x: 2, y: 4, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_moving_forward_from_the_top_edge
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 3, y: 4, direction: 'N'))
+
+    mars_rover.execute('f')
+
+    expected_coordinate = Location.new(world: world, x: 3, y: -4, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_moving_forward_past_the_top_edge
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 3, y: 3, direction: 'N'))
+
+    mars_rover.execute('fff')
+
+    expected_coordinate = Location.new(world: world, x: 3, y: -3, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_no_wrapping_when_moving_backward
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 0, y: 1, direction: 'N'))
+
+    mars_rover.execute('b')
+
+    expected_coordinate = Location.new(world: world, x: 0, y: 0, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_moving_backward_to_the_bottom_edge
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 0, y: -3, direction: 'N'))
+
+    mars_rover.execute('b')
+
+    expected_coordinate = Location.new(world: world, x: 0, y: -4, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_moving_backward_from_the_bottom_edge
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 1, y: -4, direction: 'N'))
+
+    mars_rover.execute('b')
+
+    expected_coordinate = Location.new(world: world, x: 1, y: 4, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
+
+  def test_moving_backward_past_the_bottom_edge
+    world = World.new(x_range: (-4..4), y_range: (-4..4))
+    mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 0, y: -2, direction: 'N'))
+
+    mars_rover.execute('bbbb')
+
+    expected_coordinate = Location.new(world: world, x: 0, y: 3, direction: 'N')
+    assert_located_at(expected_coordinate, mars_rover)
+  end
 end
