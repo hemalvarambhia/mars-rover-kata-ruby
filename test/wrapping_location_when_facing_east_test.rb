681f45d69e5db39dc4708a6ffa49a58afb20d7bb
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-09 22:46:56 +0000

    Removed the wrapping word from the test method names.

--- test/wrapping_location_when_facing_east_test.rb
+++ test/wrapping_location_when_facing_east_test.rb
@@ -14,7 +14,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_no_wrapping_when_moving_forward_to_the_right_hand_edge
+  def test_moving_forward_to_the_right_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 3, y: 3, direction: 'E'))
 
@@ -24,7 +24,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_wrapping_when_at_the_right_hand_edge_and_moving_forward
+  def test_moving_forward_from_the_right_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 4, y: 0, direction: 'E'))
 
@@ -34,7 +34,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_wrapping_past_the_right_hand_edge_when_moving_forward
+  def test_moving_foward_past_the_right_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 3, y: 0, direction: 'E'))
 
@@ -44,7 +44,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_starting_away_from_origin_and_wrapping_back_to_origin
+  def test_starting_away_from_origin_and_returning_to_origin
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: 2, y: 0, direction: 'E'))
 
@@ -64,7 +64,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_no_wrapping_when_moving_backward_to_the_left_hand_edge
+  def test_moving_backward_to_the_left_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: -3, y: 0, direction: 'E'))
 
@@ -74,7 +74,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_wrapping_when_at_the_left_hand_edge_and_moving_backward
+  def test_moving_backward_from_the_left_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: -4, y: 0, direction: 'E'))
 
@@ -84,7 +84,7 @@ class WrappingLocationWhenFacingEastTest < Minitest::Test
     assert_located_at(expected_coordinate, mars_rover)
   end
 
-  def test_wrapping_past_the_left_hand_edge_when_moving_backward
+  def test_moving_backward_past_the_left_hand_edge
     world = World.new(x_range: (-4..4), y_range: (-4..4))
     mars_rover = MarsRover.positioned_at(Location.new(world: world, x: -3, y: 0, direction: 'E'))
 
