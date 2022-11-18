6615bbdd490fd260accd412438b73b09e518203d
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-18 09:46:49 +0000

    Reduced the scope of some methods.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -21,11 +21,11 @@ class MarsRover
         'r' => :turn_right
       }[command]
       current_location = if instruction == :move_forward
-                           public_send(instruction)
+                           send(instruction)
                          elsif instruction == :move_backward
-                           public_send(instruction)
+                           send(instruction)
                          else
-                           public_send(instruction)
+                           send(instruction)
                          end
       @current_position = facing_direction(current_location)
     end
@@ -37,22 +37,6 @@ class MarsRover
 
   def direction
     @current_position.direction
-  end
-
-  def move_forward
-    world.next_location(@current_position)
-  end
-
-  def move_backward
-    world.previous_location(@current_position)
-  end
-
-  def turn_left
-    @current_position.public_send(:turn_left)
-  end
-
-  def turn_right
-    @current_position.public_send(:turn_right)
   end
 
   private
@@ -74,5 +58,21 @@ class MarsRover
     when 'W'
       FacingWest.new(location)
     end
+  end
+
+  def move_forward
+    world.next_location(@current_position)
+  end
+
+  def move_backward
+    world.previous_location(@current_position)
+  end
+
+  def turn_left
+    @current_position.public_send(:turn_left)
+  end
+
+  def turn_right
+    @current_position.public_send(:turn_right)
   end
 end
