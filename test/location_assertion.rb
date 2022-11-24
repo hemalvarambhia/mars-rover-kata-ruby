db9f13a332f96e261085d2aa878111f306d5a8ca
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-24 22:53:08 +0000

    Added new assertion to migrate clients to.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -1,6 +1,12 @@
 module LocationAssertion
   private
 
+  def assert_that(mars_rover, located_at:, facing:)
+    coordinates = mars_rover.position
+    assert_equal(located_at, coordinates, "Current location: #{coordinates}")
+    assert_equal(facing, mars_rover.direction, "Facing: #{mars_rover.direction}")
+  end
+
   def assert_located_at(expected_location, mars_rover)
     coordinates = mars_rover.position
     assert_equal(expected_location, coordinates, "Current location: #{coordinates}")
