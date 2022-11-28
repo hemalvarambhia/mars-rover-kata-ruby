f917ebb6f1741e657bbb94fa6ae11b42fe8b4a78
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-28 13:20:16 +0000

    Removed obsolete code.

--- test/location_assertion.rb
+++ test/location_assertion.rb
@@ -6,10 +6,4 @@ module LocationAssertion
     assert_equal(located_at, coordinates, "Current location: #{coordinates}")
     assert_equal(facing, mars_rover.direction, "Facing: #{mars_rover.direction}")
   end
-
-  def assert_located_at(expected_location, mars_rover)
-    coordinates = mars_rover.position
-    assert_equal(expected_location, coordinates, "Current location: #{coordinates}")
-    assert_equal(expected_location.direction, mars_rover.direction, "Facing: #{mars_rover.direction}")
-  end
 end
