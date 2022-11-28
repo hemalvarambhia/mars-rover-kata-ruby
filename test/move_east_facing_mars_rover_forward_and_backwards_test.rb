8ae644ce1ba8213ec1fa360086ad0ed3ade9c1c1
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-28 22:31:52 +0000

    Added a test list for moving an east facing Mars Rover forward.

--- test/move_east_facing_mars_rover_forward_and_backwards_test.rb
+++ test/move_east_facing_mars_rover_forward_and_backwards_test.rb
@@ -4,4 +4,16 @@ class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
   def test_failing_hookup
     assert_equal(4, 2 + 2)
   end
+
+  def test_no_command_moves_mars_rover_nowhere
+    skip('Test list')
+  end
+
+  def test_moving_one_step_forward_from_the_origin
+    skip('Test list')
+  end
+
+  def test_moving_one_step_forward_from_any_position
+    skip('Test list')
+  end
 end
