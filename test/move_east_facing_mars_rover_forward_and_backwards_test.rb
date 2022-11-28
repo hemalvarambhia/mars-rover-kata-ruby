0d28d66a6e7837d6efdcb2cbee78002854612e46
hemalvarambhia <h.n.varambhia@gmail.com>
2022-11-28 22:12:26 +0000

    Got the test to green.

--- test/move_east_facing_mars_rover_forward_and_backwards_test.rb
+++ test/move_east_facing_mars_rover_forward_and_backwards_test.rb
@@ -2,6 +2,6 @@ require 'minitest/autorun'
 
 class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
   def test_failing_hookup
-    assert_equal(5, 2 + 2)
+    assert_equal(4, 2 + 2)
   end
 end
