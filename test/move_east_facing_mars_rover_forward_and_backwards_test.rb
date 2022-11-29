c892bacccf00ab6f9174cd0074176d6a7fb7d8bb
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-29 16:32:16 +0000

    Removed an obsolete test.

--- test/move_east_facing_mars_rover_forward_and_backwards_test.rb
+++ test/move_east_facing_mars_rover_forward_and_backwards_test.rb
@@ -1,10 +1,6 @@
 require 'minitest/autorun'
 require_relative '../lib/mars_rover'
 class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
-  def test_failing_hookup
-    assert_equal(4, 2 + 2)
-  end
-
   def test_no_command_moves_mars_rover_nowhere
     mars_rover = MarsRover.new(x: 0, y: 0)
 
