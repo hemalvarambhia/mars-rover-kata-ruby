60b79dbea49ad0f90898cd3918ad9fe494f2f5f3
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-29 13:54:19 +0000

    We now keep the Mars Rover's position unchanged when it is initially away from the origin.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,9 +1,9 @@
 class MarsRover
   attr_reader :x, :y
 
-  def initialize
-    @x = 0
-    @y = 0
+  def initialize(x: 0, y: 0)
+    @x = x
+    @y = y
   end
 
   def execute(_command)
--- test/move_east_facing_mars_rover_forward_and_backwards_test.rb
+++ test/move_east_facing_mars_rover_forward_and_backwards_test.rb
@@ -13,6 +13,16 @@ class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
     assert_equal(0, mars_rover.y, "y-coordinate")
   end
 
+  def test_no_command_leaves_mars_rovers_position_unchanged
+    mars_rover = MarsRover.new(x: 2, y: 1)
+
+    mars_rover.execute('')
+
+    assert_equal(2, mars_rover.x, "x-coordinate")
+    assert_equal(1, mars_rover.y, "y-coordinate")
+  end
+
+
   def test_moving_one_step_forward_from_the_origin
     skip('Test list')
   end
