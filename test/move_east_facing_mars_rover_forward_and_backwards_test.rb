85b68c9d0d8d89a2b04af8711af62655a4e40a5b
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-11-29 16:36:10 +0000

    We now use the concept of coordinates.

--- test/move_east_facing_mars_rover_forward_and_backwards_test.rb
+++ test/move_east_facing_mars_rover_forward_and_backwards_test.rb
@@ -1,12 +1,20 @@
 require 'minitest/autorun'
 require_relative '../lib/mars_rover'
+class Coordinates
+  attr_reader :x, :y
+  def initialize(x:, y:)
+    @x = x
+    @y = y
+  end
+end
+
 class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
   def test_no_command_moves_mars_rover_nowhere
     mars_rover = MarsRover.new(x: 0, y: 0)
 
     mars_rover.execute('')
 
-    assert_located_at(0, 0, mars_rover)
+    assert_located_at(Coordinates.new(x: 0, y: 0), 0, 0, mars_rover)
   end
 
   def test_no_command_leaves_mars_rovers_position_unchanged
@@ -14,7 +22,7 @@ class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
 
     mars_rover.execute('')
 
-    assert_located_at(2, 1, mars_rover)
+    assert_located_at(Coordinates.new(x: 2, y: 1), 2, 1, mars_rover)
   end
 
   def test_moving_one_step_forward_from_the_origin
@@ -22,17 +30,17 @@ class MoveEastFacingMarsRoverForwardAndBackwardsTest < Minitest::Test
 
     mars_rover.execute('f')
 
-    assert_located_at(1, 0, mars_rover)
+    assert_located_at(Coordinates.new(x: 1, y: 0), 1, 0, mars_rover)
   end
 
-  def test_moving_one_step_forward_from_any_position
+  def test_moving_one_step_backwards_from_any_position
     skip('Test list')
   end
 
   private
 
-  def assert_located_at(x, y, mars_rover)
-    assert_equal(x, mars_rover.x, "x-coordinate")
-    assert_equal(y, mars_rover.y, "y-coordinate")
+  def assert_located_at(coordinates = nil, x, y, mars_rover)
+    assert_equal(coordinates.x, mars_rover.x, "x-coordinate")
+    assert_equal(coordinates.y, mars_rover.y, "y-coordinate")
   end
 end
