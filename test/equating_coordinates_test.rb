8ce377fd749e7330baa10f91e073af77bc8ce17c
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-01 20:16:50 +0000

    Added a test list for equality.

---
+++ test/equating_coordinates_test.rb
@@ -0,0 +1,35 @@
+require 'minitest/autorun'
+require_relative '../lib/coordinates'
+class EquatingCoordinatesTest < Minitest::Test
+  def test_equality_is_reflexive
+    skip('Test list')
+  end
+
+  def test_equality_is_symmetric
+    skip('Test list')
+  end
+
+  def test_equality_is_associative
+    skip('Test list')
+  end
+
+  def test_equality_is_transitive
+    skip('Test list')
+  end
+
+  def test_two_coordinates_with_different_x_coordinates_are_unequal
+    skip('Test list')
+  end
+
+  def test_two_coordinates_with_different_y_coordinates_are_unequal
+    skip('Test list')
+  end
+
+  def test_two_coordinates_with_the_same_x_and_y_coordinates_are_equal
+    skip('Test list')
+  end
+
+  def test_a_coordinate_and_an_object_of_a_different_type_are_unequal
+    skip('Test list')
+  end
+end
