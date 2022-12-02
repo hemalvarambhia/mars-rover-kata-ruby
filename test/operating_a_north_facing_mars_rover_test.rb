69fd9cc839eb97e99e751721c0cd71389b7c8480
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-02 18:13:28 +0000

    Added the next test to get passing.

--- test/operating_a_north_facing_mars_rover_test.rb
+++ test/operating_a_north_facing_mars_rover_test.rb
@@ -12,6 +12,11 @@ class OperatingANorthFacingMarsRoverTest < Minitest::Test
 
   def test_moving_one_step_forward_from_the_origin
     skip('Test list')
+    mars_rover = mars_rover_oriented_northward(Coordinates.new(x: 0, y: 0))
+
+    mars_rover.execute('')
+
+    assert_located_at(Coordinates.new(x: 0, y: 1), mars_rover)
   end
 
   def test_moving_forward_leaves_direction_unchanged
