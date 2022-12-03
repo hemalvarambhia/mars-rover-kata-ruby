f65cfe68d5057f055c255f550598fe2337d142c8
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-03 22:39:05 +0000

    We can move a south facing Mars rover one step forward.

--- test/operating_a_south_facing_mars_rover_test.rb
+++ test/operating_a_south_facing_mars_rover_test.rb
@@ -3,7 +3,11 @@ require_relative '../lib/mars_rover'
 require_relative '../lib/coordinates'
 class OperatingASouthFacingMarsRoverTest < Minitest::Test
   def test_moving_one_step_forward_from_the_origin
-    skip('Test list')
+    mars_rover = MarsRover.new(starting_position: Coordinates.new(x: 0, y: 0), orientation: 'S')
+
+    mars_rover.execute('f')
+
+    assert_located_at(Coordinates.new(x: 0, y: -1), mars_rover)
   end
 
   def test_moving_forward_leaves_orientation_unchanged
@@ -16,5 +20,11 @@ class OperatingASouthFacingMarsRoverTest < Minitest::Test
 
   def test_moving_backwards_leaves_orientation_unchanged
     skip('Test list')
+  end
+
+  private
+
+  def assert_located_at(coordinates, mars_rover)
+    assert_equal(coordinates, mars_rover.current_position, "Coordinates")
   end
 end
