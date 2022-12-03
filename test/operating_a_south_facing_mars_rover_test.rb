c56871fa2881c3d7908dc4646955c51752b2654d
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-03 22:09:51 +0000

    Added a test list.

---
+++ test/operating_a_south_facing_mars_rover_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative '../lib/mars_rover'
+require_relative '../lib/coordinates'
+class OperatingASouthFacingMarsRoverTest < Minitest::Test
+  def test_moving_one_step_backwards_from_any_position
+    skip('Test list')
+  end
+
+  def test_moving_forward_leaves_orientation_unchanged
+    skip('Test list')
+  end
+
+  def test_moving_one_step_backwards_from_any_position
+    skip('Test list')
+  end
+
+  def test_moving_backwards_leaves_orientation_unchanged
+    skip('Test list')
+  end
+end
