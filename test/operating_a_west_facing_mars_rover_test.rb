82d237f2aada2a4b171316bd7f358e860b79d801
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-05 22:35:29 +0000

    Added a test list.

---
+++ test/operating_a_west_facing_mars_rover_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+class OperatingAWestFacingMarsRoverTest < Minitest::Test
+  def test_moving_one_step_forward_from_the_origin
+    skip('Test list')
+  end
+
+  def test_moving_forward_leaves_orientation_unchanged
+    skip('Test list')
+  end
+
+  def test_moving_one_step_backwards_from_any_position
+    skip('Test list')
+  end
+
+  def test_turning_mars_rover_left_orients_it_northward
+    skip('Test list')
+  end
+end
