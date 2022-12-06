972bcce14a1a1f9bdb949611517567da27114678
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-06 15:52:26 +0000

    Test passes

--- test/moving_mars_rover_forward_on_a_planet_with_edges_test.rb
+++ test/moving_mars_rover_forward_on_a_planet_with_edges_test.rb
@@ -1,18 +1,22 @@
 require 'minitest/autorun'
 class MovingMarsRoverForwardOnAPlanetWithEdgesTest < Minitest::Test
-  def test_moving_forwards_to_the_north_edge
+  def test_moving_forwards_to_the_northern_edge
     skip('Test list')
   end
 
-  def test_moving_forwards_from_the_north_edge
+  def test_moving_forwards_from_the_northern_edge
     skip('Test list')
   end
 
-  def test_moving_forwards_past_the_north_edge
+  def test_moving_forwards_past_the_northern_edge
     skip('Test list')
   end
 
   def test_moving_forwards_to_the_eastern_edge
+    skip('Test list')
+  end
+
+  def test_moving_forwards_from_the_eastern_edge
     skip('Test list')
   end
 end
