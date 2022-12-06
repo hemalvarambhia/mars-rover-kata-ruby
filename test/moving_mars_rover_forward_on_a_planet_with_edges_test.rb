2905c5a545856d2cbc70f8f1cfefb20e97916506
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-06 21:36:43 +0000

    We can move a north facing Mars rover to the northern edge of a planet.

--- test/moving_mars_rover_forward_on_a_planet_with_edges_test.rb
+++ test/moving_mars_rover_forward_on_a_planet_with_edges_test.rb
@@ -1,7 +1,14 @@
 require 'minitest/autorun'
+require_relative './mars_rover_in_cartesian_coordinate_system'
 class MovingMarsRoverForwardOnAPlanetWithEdgesTest < Minitest::Test
+  include CoordinatesAssertion
+
   def test_moving_forwards_to_the_northern_edge
-    skip('Test list')
+    mars_rover = mars_rover_oriented_northward(Coordinates.new(x: 1, y: 4))
+
+    mars_rover.execute('f')
+
+    assert_located_at(Coordinates.new(x: 1, y: 5), mars_rover)
   end
 
   def test_moving_forwards_from_the_northern_edge
@@ -18,5 +25,11 @@ class MovingMarsRoverForwardOnAPlanetWithEdgesTest < Minitest::Test
 
   def test_moving_forwards_from_the_eastern_edge
     skip('Test list')
+  end
+
+  private
+
+  def mars_rover_oriented_northward(starting_position)
+    MarsRover.new(starting_position: starting_position, orientation: 'N')
   end
 end
