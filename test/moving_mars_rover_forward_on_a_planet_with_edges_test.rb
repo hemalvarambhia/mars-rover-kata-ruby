edbafe9bfd34034d5a04d499368b2ac9e16df414
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-06 15:48:33 +0000

    Added a test list for moving a Mars rover on a planet with edges.

---
+++ test/moving_mars_rover_forward_on_a_planet_with_edges_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+class MovingMarsRoverForwardOnAPlanetWithEdgesTest < Minitest::Test
+  def test_moving_forwards_to_the_north_edge
+    skip('Test list')
+  end
+
+  def test_moving_forwards_from_the_north_edge
+    skip('Test list')
+  end
+
+  def test_moving_forwards_past_the_north_edge
+    skip('Test list')
+  end
+
+  def test_moving_forwards_to_the_eastern_edge
+    skip('Test list')
+  end
+end
