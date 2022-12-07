f9d348929826c4b20195446416c896b51307674f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-07 16:58:17 +0000

    Pushed details up the callstack.

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,9 +1,9 @@
 class Planet
   attr_reader :northern_edge, :southern_edge
 
-  def initialize
-    @northern_edge = 5
-    @southern_edge = -5
+  def initialize(northern_edge: 5, southern_edge: -5)
+    @northern_edge = northern_edge
+    @southern_edge = southern_edge
   end
 
   def at_northern_edge?(location)
