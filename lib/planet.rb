021be410fc991bcd1251ecf3253ff4f356896f5a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-07 17:00:11 +0000

    No need for the default setting.

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,7 +1,7 @@
 class Planet
   attr_reader :northern_edge, :southern_edge
 
-  def initialize(northern_edge: 5, southern_edge: -5)
+  def initialize(northern_edge:, southern_edge:)
     @northern_edge = northern_edge
     @southern_edge = southern_edge
   end
