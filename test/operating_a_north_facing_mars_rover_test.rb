ce2b90a2ff5236861fd973cfd3fc9bf19ff07aff
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-07 16:28:36 +0000

    Copied the tests to a more sensible place.

--- test/operating_a_north_facing_mars_rover_test.rb
+++ test/operating_a_north_facing_mars_rover_test.rb
@@ -91,6 +91,34 @@ class OperatingANorthFacingMarsRoverTest < Minitest::Test
     assert_located_at(Coordinates.new(x: 2, y: -4), mars_rover)
   end
 
+  def test_moving_forwards_to_the_northern_edge
+    mars_rover = mars_rover_oriented_northward(Coordinates.new(x: 1, y: 4))
+
+    mars_rover.execute('f')
+
+    assert_located_at(Coordinates.new(x: 1, y: 5), mars_rover)
+  end
+
+  def test_moving_forwards_from_the_northern_edge
+    mars_rover = mars_rover_oriented_northward(Coordinates.new(x: 1, y: 5))
+
+    mars_rover.execute('f')
+
+    assert_located_at(Coordinates.new(x: 1, y: -5), mars_rover)
+  end
+
+  def test_moving_forwards_past_the_northern_edge
+    skip('Test list')
+  end
+
+  def test_moving_forwards_to_the_eastern_edge
+    skip('Test list')
+  end
+
+  def test_moving_forwards_from_the_eastern_edge
+    skip('Test list')
+  end
+
   private
 
   def mars_rover_oriented_northward(starting_position)
