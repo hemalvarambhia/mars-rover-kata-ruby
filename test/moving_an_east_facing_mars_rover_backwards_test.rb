163412f00216cf6b8f9581d58e9e2d01aef98ab2
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-13 13:51:54 +0000

    Clarified the name of the test.

--- test/moving_an_east_facing_mars_rover_backwards_test.rb
+++ test/moving_an_east_facing_mars_rover_backwards_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative './mars_rover_in_cartesian_coordinate_system'
 
-class OperatingAnEastFacingMarsRoverTest < Minitest::Test
+class MovingAnEastFacingMarsRoverBackwardsTest < Minitest::Test
   include CoordinatesAssertion
 
   def test_moving_one_step_backwards_from_any_position
