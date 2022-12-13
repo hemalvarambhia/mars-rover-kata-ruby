29b489943a61c527bb52fc230f05e220661bb6a1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2022-12-13 13:57:12 +0000

    Added a test list for moving an east facing Mars rover when there are obstacles.

--- test/moving_an_east_facing_mars_rover_forwards_test.rb
+++ test/moving_an_east_facing_mars_rover_forwards_test.rb
@@ -73,6 +73,18 @@ class MovingAnEastFacingMarsRoverForwardsTest < Minitest::Test
     assert_located_at(Coordinates.new(x: -3, y: -2), mars_rover)
   end
 
+  def test_mars_rover_cannot_move_forward_when_obstacle_is_in_front
+    skip('Test list')
+  end
+
+  def test_mars_rover_cannot_move_past_eastern_edge_when_obstacle_is_located_on_western_edge
+    skip('Test list')
+  end
+
+  def test_mars_rover_can_move_forward_up_to_an_obstacle_and_not_beyond
+    skip('Test list')
+  end
+
   private
 
   def mars_rover_oriented_eastward(planet, starting_position)
