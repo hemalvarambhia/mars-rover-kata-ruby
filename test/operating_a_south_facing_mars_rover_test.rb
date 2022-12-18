c4e002bbf4bfe1bf179b544fc7a50621cf03109c
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-18 16:18:08 +0000

    Confirmed that we can move up to an obstable but never beyond.

--- test/operating_a_south_facing_mars_rover_test.rb
+++ test/operating_a_south_facing_mars_rover_test.rb
@@ -153,7 +153,12 @@ class OperatingASouthFacingMarsRoverTest < Minitest::Test
   end
 
   def test_mars_rover_cannot_move_backwards_up_to_an_obstacle_and_not_beyond
-    skip('Test list')
+    planet = Planet.new(northern_edge: 6, eastern_edge: nil, southern_edge: -6, western_edge: nil, obstacles: [Coordinates.new(x: 3, y: 1)])
+    mars_rover = south_facing_mars_rover(planet, Coordinates.new(x: 3, y: -3))
+
+    mars_rover.execute('bbbbb')
+
+    assert_located_at(Coordinates.new(x: 3, y: 0), mars_rover)
   end
 
   def test_mars_rover_cannot_move_past_northern_edge_when_obstacle_is_located_on_southern_edge
