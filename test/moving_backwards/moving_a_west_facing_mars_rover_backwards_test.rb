be574c7019341931ce4d8fa65b69fd1750ed188e
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-25 10:30:26 +0000

    Moved some tests to a more sensible place.

---
+++ test/moving_backwards/moving_a_west_facing_mars_rover_backwards_test.rb
@@ -0,0 +1,39 @@
+require 'minitest/autorun'
+require_relative '../mars_rover_in_cartesian_coordinate_system'
+class MovingAWestFacingMarsRoverBackwardsTest < Minitest::Test
+  include CoordinatesAssertion
+
+  def test_moving_one_step_backwards_from_any_position
+    planet = Planet.new(northern_edge: 5, eastern_edge: 5, southern_edge: -5, western_edge: -5)
+    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 0, y: 0))
+
+    mars_rover.execute('b')
+
+    assert_located_at(Coordinates.new(x: 1, y: 0), mars_rover)
+  end
+
+  def test_moving_backwards_from_the_eastern_edge
+    planet = Planet.new(northern_edge: nil, eastern_edge: 4, southern_edge: nil, western_edge: -4)
+    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 4, y: 0))
+
+    mars_rover.execute('b')
+
+    assert_located_at(Coordinates.new(x: -4, y: 0), mars_rover)
+  end
+
+  def test_moving_backwards_past_the_eastern_edge
+    planet = Planet.new(northern_edge: nil, eastern_edge: 5, southern_edge: nil, western_edge: -4)
+    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 3, y: 0))
+
+    mars_rover.execute('bbbb')
+
+    assert_located_at(Coordinates.new(x: -3, y: 0), mars_rover)
+  end
+
+  private
+
+  def west_facing_mars_rover(planet, starting_position)
+    mars_rover = MarsRover.new(planet: planet, starting_position: starting_position, orientation: 'W')
+    VehicleController.new(vehicle: mars_rover)
+  end
+end
--- test/operating_a_west_facing_mars_rover_test.rb
+++ test/operating_a_west_facing_mars_rover_test.rb
@@ -3,15 +3,6 @@ require_relative './mars_rover_in_cartesian_coordinate_system'
 class OperatingAWestFacingMarsRoverTest < Minitest::Test
   include CoordinatesAssertion
 
-  def test_moving_one_step_backwards_from_any_position
-    planet = Planet.new(northern_edge: 5, eastern_edge: 5, southern_edge: -5, western_edge: -5)
-    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 0, y: 0))
-
-    mars_rover.execute('b')
-
-    assert_located_at(Coordinates.new(x: 1, y: 0), mars_rover)
-  end
-
   def test_turning_mars_rover_left_orients_it_southward
     mars_rover = west_facing_mars_rover(nil,nil)
 
@@ -42,24 +33,6 @@ class OperatingAWestFacingMarsRoverTest < Minitest::Test
     mars_rover.execute('r')
 
     assert_located_at(Coordinates.new(x: -1, y: -3), mars_rover)
-  end
-
-  def test_moving_backwards_from_the_eastern_edge
-    planet = Planet.new(northern_edge: nil, eastern_edge: 4, southern_edge: nil, western_edge: -4)
-    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 4, y: 0))
-
-    mars_rover.execute('b')
-
-    assert_located_at(Coordinates.new(x: -4, y: 0), mars_rover)
-  end
-
-  def test_moving_backwards_past_the_eastern_edge
-    planet = Planet.new(northern_edge: nil, eastern_edge: 5, southern_edge: nil, western_edge: -4)
-    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 3, y: 0))
-
-    mars_rover.execute('bbbb')
-
-    assert_located_at(Coordinates.new(x: -3, y: 0), mars_rover)
   end
 
   private
