4d647908d18a81a6fda2312a9e2fbc593c0f6aea
hemalvarambhia <h.n.varambhia@gmail.com>
2022-12-25 10:22:53 +0000

    Copied the forward movement tests to a more sensible place.

--- test/operating_a_west_facing_mars_rover_test.rb
+++ test/operating_a_west_facing_mars_rover_test.rb
@@ -3,24 +3,6 @@ require_relative './mars_rover_in_cartesian_coordinate_system'
 class OperatingAWestFacingMarsRoverTest < Minitest::Test
   include CoordinatesAssertion
 
-  def test_moving_one_step_forward_from_the_origin
-    planet = Planet.new(northern_edge: 5, eastern_edge: 5, southern_edge: -5, western_edge: -5)
-    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 2, y: 2))
-
-    mars_rover.execute('f')
-
-    assert_located_at(Coordinates.new(x: 1, y: 2), mars_rover)
-  end
-
-  def test_moving_forward_leaves_orientation_unchanged
-    planet = Planet.new(northern_edge: 5, eastern_edge: 5, southern_edge: -5, western_edge: -5)
-    mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 2, y: 2))
-
-    mars_rover.execute('f')
-
-    assert_equal('W', mars_rover.orientation)
-  end
-
   def test_moving_one_step_backwards_from_any_position
     planet = Planet.new(northern_edge: 5, eastern_edge: 5, southern_edge: -5, western_edge: -5)
     mars_rover = west_facing_mars_rover(planet, Coordinates.new(x: 0, y: 0))
