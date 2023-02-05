539477f3454e9476f1c9dcc9d755fffe4e9d74c3
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-05 14:16:30 +0000

    Added a test list for a north facing vehicle on a spherical planet.

---
+++ test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+class MovingANorthFacingVehicleOnASphericalPlanetTest < Minitest::Test
+  def test_moving_forward_when_facing_north_far_from_north_pole
+    skip('Test list')
+  end
+
+  def test_moving_forward_on_to_the_north_pole
+    skip('Test list')
+  end
+
+  def test_moving_two_steps_forward_far_from_north_pole
+    skip('Test list')
+  end
+end
