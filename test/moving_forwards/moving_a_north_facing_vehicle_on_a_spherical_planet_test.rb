458af764d927e8e7bb4587b22587e75a92ff3986
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-12 15:18:43 +0000

    Added a test to demonstrate moving forward on a spherical planet.

--- test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
+++ test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
@@ -1,7 +1,25 @@
 require 'minitest/autorun'
+require 'ostruct'
+require_relative '../../lib/vehicle_controller'
+SphericalPlanet = Class.new
+class PolarCoordinates
+  attr_reader :longitude, :latitude
+  def initialize(longitude:, latitude:); end
+end
 class MovingANorthFacingVehicleOnASphericalPlanetTest < Minitest::Test
   def test_moving_forward_when_facing_north_far_from_north_pole
     skip('Test list')
+    spherical_planet = SphericalPlanet.new
+    starting_position = PolarCoordinates.new(longitude: 1, latitude: 1)
+    mars_rover =
+      VehicleController.new(
+        vehicle: MarsRover.new(planet: spherical_planet, starting_position: starting_position, orientation: 'N')
+      )
+
+    mars_rover.execute('f')
+
+    assert_equal(1, mars_rover.current_position.longitude)
+    assert_equal(2, mars_rover.current_position.latitude)
   end
 
   def test_moving_forward_on_to_the_north_pole
