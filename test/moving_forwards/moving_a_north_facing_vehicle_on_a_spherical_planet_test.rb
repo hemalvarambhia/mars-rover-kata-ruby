c5d7efc2bf845f6132f945b154202b0bca4df1ac
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-13 21:50:05 +0000

    Fake it til we make it - we can move forward on a Spherical Planet.

--- test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
+++ test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
@@ -1,14 +1,20 @@
 require 'minitest/autorun'
 require 'ostruct'
 require_relative '../../lib/vehicle_controller'
-SphericalPlanet = Class.new
+class SphericalPlanet
+  def next_location_to(location, orientation)
+    PolarCoordinates.new(longitude: 1, latitude: 2)
+  end
+end
 class PolarCoordinates
   attr_reader :longitude, :latitude
-  def initialize(longitude:, latitude:); end
+  def initialize(longitude:, latitude:)
+    @longitude = longitude
+    @latitude = latitude
+  end
 end
 class MovingANorthFacingVehicleOnASphericalPlanetTest < Minitest::Test
   def test_moving_forward_when_facing_north_far_from_north_pole
-    skip('Test list')
     spherical_planet = SphericalPlanet.new
     starting_position = PolarCoordinates.new(longitude: 1, latitude: 1)
     mars_rover =
@@ -20,6 +26,7 @@ class MovingANorthFacingVehicleOnASphericalPlanetTest < Minitest::Test
 
     assert_equal(1, mars_rover.current_position.longitude)
     assert_equal(2, mars_rover.current_position.latitude)
+    assert_equal('N', mars_rover.orientation)
   end
 
   def test_moving_forward_on_to_the_north_pole
