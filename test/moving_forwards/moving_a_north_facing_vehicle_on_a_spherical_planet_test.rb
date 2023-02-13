97b07e86e64474c3343f48a4c92f837d4c1a8f16
hemalvarambhia <h.n.varambhia@gmail.com>
2023-02-13 23:07:36 +0000

    Added another test in order to triangulate.

--- test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
+++ test/moving_forwards/moving_a_north_facing_vehicle_on_a_spherical_planet_test.rb
@@ -29,8 +29,20 @@ class MovingANorthFacingVehicleOnASphericalPlanetTest < Minitest::Test
     assert_equal('N', mars_rover.orientation)
   end
 
-  def test_moving_forward_on_to_the_north_pole
+  def test_moving_forward_from_any_point_far_from_north_pole
     skip('Test list')
+    spherical_planet = SphericalPlanet.new
+    starting_position = PolarCoordinates.new(longitude: 2, latitude: 1)
+    mars_rover =
+      VehicleController.new(
+        vehicle: MarsRover.new(planet: spherical_planet, starting_position: starting_position, orientation: 'N')
+      )
+
+    mars_rover.execute('f')
+
+    assert_equal(2, mars_rover.current_position.longitude, 'longitude')
+    assert_equal(2, mars_rover.current_position.latitude, 'latitude')
+    assert_equal('S', mars_rover.orientation)
   end
 
   def test_moving_two_steps_forward_far_from_north_pole
