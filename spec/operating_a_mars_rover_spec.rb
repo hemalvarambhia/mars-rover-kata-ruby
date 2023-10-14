8162528d04b0a99e3174b0b3c6613c676ab861a9
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-14 23:13:39 +0100

    Parallel Change: add a new concept of coordinates.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -1,9 +1,17 @@
 RSpec.describe 'Operating a Mars rover' do
+  class Coordinate
+    attr_reader :x, :y
+    def initialize(x:, y:)
+      @x = x
+      @y = y
+    end
+  end
   class MarsRover
     attr_reader :x, :y
     def initialize(x: 0, y: 0)
       @x = x
       @y = y
+      @current_position = Coordinate.new(x: x, y: y)
     end
   end
 
