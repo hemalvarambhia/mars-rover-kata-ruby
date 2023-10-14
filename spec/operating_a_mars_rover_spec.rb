0d98e4c09c54acde6cdb2de626c96e34cc55b563
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-14 23:04:50 +0100

    Now a Mars rover can start anywhere on the planet.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'Operating a Mars rover' do
     attr_reader :x, :y
     def initialize(x: 0, y: 0)
       @x = x
-      @y = 0
+      @y = y
     end
   end
 
@@ -15,11 +15,10 @@ RSpec.describe 'Operating a Mars rover' do
   end
 
   it 'has a starting position anywhere on the planet' do
-    pending('TODO')
     mars_rover = MarsRover.new(x: 2, y: 3)
 
     expect(mars_rover.x).to(eq(2), "Expected Mars Rover's x-coordinate to be 2, but got #{mars_rover.x}")
-    expect(mars_rover.y).to(eq(3), "Expected Mars Rover's x-coordinate to be 3, but got #{mars_rover.y}")
+    expect(mars_rover.y).to(eq(3), "Expected Mars Rover's y-coordinate to be 3, but got #{mars_rover.y}")
   end
 
   it 'has a direction that it is facing'
