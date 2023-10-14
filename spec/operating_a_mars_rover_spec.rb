0dc1a70bf66037c100840983de5c6743013d81cf
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-14 23:20:42 +0100

    Made similar code even more similar.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -24,15 +24,17 @@ RSpec.describe 'Operating a Mars rover' do
   it 'has a starting position at the origin' do
     mars_rover = MarsRover.new(x: 0, y: 0)
 
-    expect(mars_rover.x).to(eq(0), "Expected Mars Rover's x-coordinate to be 0, but got #{mars_rover.x}")
-    expect(mars_rover.y).to(eq(0), "Expected Mars Rover's y-coordinate to be 0, but got #{mars_rover.y}")
+    expected_coordinate = Coordinate.new(x: 0, y: 0)
+    expect(mars_rover.x).to(eq(expected_coordinate.x), "Expected Mars Rover's x-coordinate to be 0, but got #{mars_rover.x}")
+    expect(mars_rover.y).to(eq(expected_coordinate.y), "Expected Mars Rover's y-coordinate to be 0, but got #{mars_rover.y}")
   end
 
   it 'has a starting position anywhere on the planet' do
     mars_rover = MarsRover.new(x: 2, y: 3)
 
-    expect(mars_rover.x).to(eq(2), "Expected Mars Rover's x-coordinate to be 2, but got #{mars_rover.x}")
-    expect(mars_rover.y).to(eq(3), "Expected Mars Rover's y-coordinate to be 3, but got #{mars_rover.y}")
+    expected_coordinate = Coordinate.new(x: 2, y: 3)
+    expect(mars_rover.x).to(eq(expected_coordinate.x), "Expected Mars Rover's x-coordinate to be 2, but got #{mars_rover.x}")
+    expect(mars_rover.y).to(eq(expected_coordinate.y), "Expected Mars Rover's y-coordinate to be 3, but got #{mars_rover.y}")
   end
 
   it 'has a direction that it is facing'
