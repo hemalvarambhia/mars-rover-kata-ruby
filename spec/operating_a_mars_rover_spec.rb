193f36df30cc19f86463120cf7e1541c36777e7c
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-14 22:56:54 +0100

    We can place a Mars rover at a defined origin.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -1,4 +1,22 @@
 RSpec.describe 'Operating a Mars rover' do
-  it 'has a starting position at the origin'
+  class MarsRover
+    def initialize(x: 0, y: 0)
+    end
+
+    def x
+      0
+    end
+
+    def y
+      0
+    end
+  end
+
+  it 'has a starting position at the origin' do
+    mars_rover = MarsRover.new(x: 0, y: 0)
+
+    expect(mars_rover.x).to eq(0)
+    expect(mars_rover.y).to eq(0)
+  end
   it 'has a direction that it is facing'
 end
