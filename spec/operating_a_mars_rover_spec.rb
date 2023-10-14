da14d0cfa0a374b5402689f4aeb74fd19bb4c744
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-14 23:03:06 +0100

    Added the next test to get passing.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe 'Operating a Mars rover' do
   class MarsRover
     attr_reader :x, :y
     def initialize(x: 0, y: 0)
-      @x = 0
+      @x = x
       @y = 0
     end
   end
@@ -13,5 +13,14 @@ RSpec.describe 'Operating a Mars rover' do
     expect(mars_rover.x).to eq(0)
     expect(mars_rover.y).to eq(0)
   end
+
+  it 'has a starting position anywhere on the planet' do
+    pending('TODO')
+    mars_rover = MarsRover.new(x: 2, y: 3)
+
+    expect(mars_rover.x).to(eq(2), "Expected Mars Rover's x-coordinate to be 2, but got #{mars_rover.x}")
+    expect(mars_rover.y).to(eq(3), "Expected Mars Rover's x-coordinate to be 3, but got #{mars_rover.y}")
+  end
+
   it 'has a direction that it is facing'
 end
