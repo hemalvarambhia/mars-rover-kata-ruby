281be42b4a8dc5b5c6e4a3641c8a5d89504d6629
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-15 15:58:30 +0100

    Replaced the method with idiomatic ruby (attr_reader).

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -1,11 +1,9 @@
 class MarsRover
+  attr_reader :direction
+
   def initialize(x: 0, y: 0)
     @current_position = Coordinate.new(x: x, y: y)
     @direction = 'S'
-  end
-
-  def direction
-    @direction
   end
 
   def x
