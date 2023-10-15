98278dc378cdc8a1e37d9d318f5402c453de78b6
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-15 22:58:26 +0100

    Improved the description of the behaviour.

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -1,8 +1,8 @@
 require_relative '../lib/coordinates'
 describe Coordinates do
   describe 'equals' do
-    example 'two coordinates with different x-coordinates are unequal'
-    example 'two coordinates with different y-coordinates are unequal'
+    example 'two coordinates with different x-coordinates but same y-coordinates are unequal'
+    example 'two coordinates with different y-coordinates but same x-coordinates are unequal'
     example 'two coordinates with the x and y-coordinates are equal' do
       coordinates = Coordinates.new(x: -1, y: -2)
       equal_coordinates = Coordinates.new(x: -1, y: -2)
