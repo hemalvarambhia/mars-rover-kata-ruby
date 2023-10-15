52460a25e723861616e90cd9d76daaf252c2ec75
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-15 22:57:33 +0100

    Two coordinates with the same x and y-coordinates are equal.

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -3,7 +3,11 @@ describe Coordinates do
   describe 'equals' do
     example 'two coordinates with different x-coordinates are unequal'
     example 'two coordinates with different y-coordinates are unequal'
-    example 'two coordinates with the x and y-coordinates are equal'
+    example 'two coordinates with the x and y-coordinates are equal' do
+      coordinates = Coordinates.new(x: -1, y: -2)
+      equal_coordinates = Coordinates.new(x: -1, y: -2)
+      expect(coordinates).to eq(equal_coordinates)
+    end
     example 'a coordinate and an object of a different type are unequal'
   end
 end
