c1283d019b81c7c61ead78894f74393310008d31
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-15 15:53:44 +0100

    Removed duplicate code.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -1,19 +1,6 @@
 require_relative '../lib/coordinates'
 require_relative '../lib/mars_rover'
 RSpec.describe 'Operating a Mars rover' do
-  class MarsRover
-    def initialize(x: 0, y: 0)
-      @current_position = Coordinate.new(x: x, y: y)
-    end
-
-    def x
-      @current_position.x
-    end
-
-    def y
-      @current_position.y
-    end
-  end
 
   it 'has a starting position at the origin' do
     mars_rover = MarsRover.new(x: 0, y: 0)
