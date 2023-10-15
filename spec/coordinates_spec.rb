a1a1548216c56bd7ec419b200b30ad4cd34fd1d5
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-15 23:00:56 +0100

    Two coordinates with different x-coordinates are unequal.

--- spec/coordinates_spec.rb
+++ spec/coordinates_spec.rb
@@ -1,11 +1,17 @@
 require_relative '../lib/coordinates'
 describe Coordinates do
   describe 'equals' do
-    example 'two coordinates with different x-coordinates but same y-coordinates are unequal'
+    example 'two coordinates with different x-coordinates but same y-coordinates are unequal' do
+      coordinates = Coordinates.new(x: -1, y: -2)
+      coordinates_with_different_x = Coordinates.new(x: 3, y: -2)
+
+      expect(coordinates).not_to eq(coordinates_with_different_x)
+    end
     example 'two coordinates with different y-coordinates but same x-coordinates are unequal'
     example 'two coordinates with the x and y-coordinates are equal' do
       coordinates = Coordinates.new(x: -1, y: -2)
       equal_coordinates = Coordinates.new(x: -1, y: -2)
+
       expect(coordinates).to eq(equal_coordinates)
     end
     example 'a coordinate and an object of a different type are unequal'
