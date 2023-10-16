88b89e64fa6720fc1ce3151739783aa7b992f586
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-16 19:12:51 +0100

    Extracted the code to an intention-revealing method.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -9,20 +9,26 @@ class MarsRover
 
   def execute(commands_from_earth)
     commands_from_earth.each do |_|
-      case @direction
-      when 'N'
-        @current_position = Coordinates.new(x: @current_position.x, y: @current_position.y + 1)
-      when 'E'
-        @current_position = Coordinates.new(x: @current_position.x + 1, y: @current_position.y)
-      when 'S'
-        @current_position = Coordinates.new(x: @current_position.x, y: @current_position.y - 1)
-      when 'W'
-        @current_position = Coordinates.new(x: @current_position.x - 1, y: @current_position.y)
-      end
+      move_forwards
     end
   end
 
   def inspect
     "a Mars Rover at #{@current_position.inspect} facing #{direction}"
+  end
+
+  private
+
+  def move_forwards
+    case @direction
+    when 'N'
+      @current_position = Coordinates.new(x: @current_position.x, y: @current_position.y + 1)
+    when 'E'
+      @current_position = Coordinates.new(x: @current_position.x + 1, y: @current_position.y)
+    when 'S'
+      @current_position = Coordinates.new(x: @current_position.x, y: @current_position.y - 1)
+    when 'W'
+      @current_position = Coordinates.new(x: @current_position.x - 1, y: @current_position.y)
+    end
   end
 end
