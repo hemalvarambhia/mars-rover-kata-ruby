8f43bc90e8361946918514ae04fc006a583b4c18
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-16 19:31:06 +0100

    Improved some code formatting.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -110,8 +110,6 @@ RSpec.describe 'Operating a Mars rover' do
   end
 
   RSpec::Matchers.define :be_located_at do |expected_position|
-    match do |rover|
-      rover.current_position == expected_position
-    end
+    match { |rover| rover.current_position == expected_position }
   end
 end
