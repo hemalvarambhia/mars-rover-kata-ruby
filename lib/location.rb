1c40d93c4cc09de87de6546ca3e73350debf8e0a
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-27 09:44:19 +0100

    Refactor/Move Method: copied the logic for moving forwards to the location class and made it compute a new location.

--- lib/location.rb
+++ lib/location.rb
@@ -4,4 +4,29 @@ class Location
     @coordinates = coordinates
     @direction = direction
   end
+
+  def forwards
+    case @direction
+    when 'N'
+      Location.new(
+        coordinates: Coordinates.new(x: @coordinates.x, y: @coordinates.y + 1),
+        direction: @direction
+      )
+    when 'E'
+      Location.new(
+        coordinates: Coordinates.new(x: @coordinates.x + 1, y: @coordinates.y),
+        direction: @direction
+      )
+    when 'S'
+      Location.new(
+        coordinates: Coordinates.new(x: @coordinates.x, y: @coordinates.y - 1),
+        direction: @direction
+      )
+    when 'W'
+      Location.new(
+        coordinates: Coordinates.new(x: @coordinates.x - 1, y: @coordinates.y),
+        direction: @direction
+      )
+    end
+  end
 end
