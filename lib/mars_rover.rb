d93d3c5e1d39356ccbcce2370b5836078609ded5
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2023-10-27 16:48:50 +0100

    Improved code formatting.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -6,19 +6,20 @@ class MarsRover
   end
 
   def execute(commands_from_earth)
-    @current_location = commands_from_earth.inject(@current_location) do |location, command|
-      case command
-      when 'f'
-        location.forwards
-      when 'b'
-        location.backwards
-      when 'l'
-        location.rotate_left
-      when 'r'
-        location.rotate_right
-      else
-        location
-      end
+    @current_location =
+      commands_from_earth.inject(@current_location) do |location, command|
+        case command
+        when 'f'
+          location.forwards
+        when 'b'
+          location.backwards
+        when 'l'
+          location.rotate_left
+        when 'r'
+          location.rotate_right
+        else
+          location
+        end
     end
   end
 
