cb8442f8959675d166300235be9b7a122b1673ec
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-28 20:39:50 +0100

    Added the location of the north pole to the tests.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -91,9 +91,9 @@ RSpec.describe 'Operating a Mars rover' do
 
     describe 'Travelling to the north pole' do
       it 'faces south when it reaches the north pole' do
-        pending('Discussion with domain expert')
+        pending('Pending discussion with domain expert')
         north_pole = Coordinates.new(x: 0, y: 9)
-        starting_location = Location.new(direction: 'N', coordinates: Coordinates.new(x: 0, y: 8))
+        starting_location = Location.new(north_pole: north_pole, direction: 'N', coordinates: Coordinates.new(x: 0, y: 8))
         mars_rover = MarsRover.new(starting_location: starting_location)
 
         mars_rover.execute(['f'])
@@ -104,7 +104,7 @@ RSpec.describe 'Operating a Mars rover' do
       it 'moves to the same latitude in the other half of the planet' do
         pending('Discussion with domain expert')
         north_pole = Coordinates.new(x: 0, y: 9)
-        starting_location = Location.new(direction: 'N', coordinates: Coordinates.new(x: 0, y: 8))
+        starting_location = Location.new(north_pole: north_pole, direction: 'N', coordinates: Coordinates.new(x: 0, y: 8))
         mars_rover = MarsRover.new(starting_location: starting_location)
 
         mars_rover.execute(['f'])
