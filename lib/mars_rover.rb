60daacbba430dcdc08fd18378cc6bf33f7d9ee57
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-30 22:09:01 +0000

    Refactor: stop trying to hit me and hit me.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -42,15 +42,15 @@ class MarsRover
   private
 
   def corrected_for_north_pole(location)
-    if at_north_pole?(location)
+    if at_north_pole?(location.coordinates)
       Location.new(coordinates: Coordinates.new(x: 18, y: location.coordinates.y), direction: 'S').forwards
     else
       location
     end
   end
 
-  def at_north_pole?(location)
+  def at_north_pole?(coordinates)
     north_pole = Coordinates.new(x: 0, y: 9)
-    location.coordinates == north_pole
+    coordinates == north_pole
   end
 end
