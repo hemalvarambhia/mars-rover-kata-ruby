31e5047950adecfdfdbe4ade3c5a7998def22e74
hemalvarambhia <h.n.varambhia@gmail.com>
2023-10-30 22:21:33 +0000

    Refactor/Extract Field so we can push a detail up the callstack.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -42,16 +42,7 @@ class MarsRover
   private
 
   def with_north_pole_correction(location)
-    if at_north_pole?(location.coordinates)
-      Location.new(coordinates: Coordinates.new(x: 18, y: location.coordinates.y), direction: 'S').forwards
-    else
-      location
-    end
-    Map.new.with_north_pole_correction(location)
-  end
-
-  def at_north_pole?(coordinates)
-    north_pole = Coordinates.new(x: 0, y: 9)
-    coordinates == north_pole
+    @map = Map.new
+    @map.with_north_pole_correction(location)
   end
 end
