648b757e56350f25dd2898463ce4c074c9a71e9d
hemalvarambhia <h.n.varambhia@gmail.com>
2023-11-01 14:28:21 +0000

    Behaviour change: just move to the north pole, face south and a longitude 180 degrees to the current one.

--- lib/map.rb
+++ lib/map.rb
@@ -3,7 +3,7 @@ require 'location'
 class Map
   def with_north_pole_correction(location)
     if at_north_pole?(location.coordinates)
-      Location.new(coordinates: Coordinates.new(x: location.coordinates.x + 18, y: location.coordinates.y), direction: 'S').forwards
+      Location.new(coordinates: Coordinates.new(x: location.coordinates.x + 18, y: location.coordinates.y), direction: 'S')
     else
       location
     end
