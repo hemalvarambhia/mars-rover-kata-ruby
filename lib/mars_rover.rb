59ac3819a198aec3d2bdbf32a0c19ca676e80e3a
hemalvarambhia <h.n.varambhia@gmail.com>
2023-11-18 15:40:36 +0000

    Refactor: Made the ordering of the methods more intuitive.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -51,19 +51,19 @@ class MarsRover
     with_south_pole_correction(new_location)
   end
 
-  def with_south_pole_correction(location)
-    at_south_pole = location.coordinates.y == SOUTH_POLE_LATITUDE
-    if at_south_pole
-      Location.north_facing(Coordinates.new(x: (location.coordinates.x + 18) % 36, y: location.coordinates.y + 1))
+  def with_north_pole_correction(location)
+    at_north_pole = (location.coordinates.y == NORTH_POLE_LATITUDE)
+    if at_north_pole
+      Location.south_facing(Coordinates.new(x: (location.coordinates.x + 18) % 36, y: location.coordinates.y - 1))
     else
       location
     end
   end
 
-  def with_north_pole_correction(location)
-    at_north_pole = (location.coordinates.y == NORTH_POLE_LATITUDE)
-    if at_north_pole
-      Location.south_facing(Coordinates.new(x: (location.coordinates.x + 18) % 36, y: location.coordinates.y - 1))
+  def with_south_pole_correction(location)
+    at_south_pole = location.coordinates.y == SOUTH_POLE_LATITUDE
+    if at_south_pole
+      Location.north_facing(Coordinates.new(x: (location.coordinates.x + 18) % 36, y: location.coordinates.y + 1))
     else
       location
     end
