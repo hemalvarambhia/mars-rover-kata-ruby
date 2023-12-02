2d25cd0761232c09882283a374a77c6506e47a0b
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-02 21:16:04 +0000

    refactor: duplicated the forward logic to Map.

--- lib/non_polar_map.rb
+++ lib/non_polar_map.rb
@@ -1,6 +1,15 @@
 class NonPolarMap
   def forwards(location)
-    location.forwards
+    case location.direction
+    when 'N'
+      Location.new(coordinates: Coordinates.new(x: location.x, y: location.y + 1), direction: location.direction)
+    when 'E'
+      Location.new(coordinates: Coordinates.new(x: (location.x + 1) % 36, y: location.y), direction: location.direction)
+    when 'S'
+      Location.new(coordinates: Coordinates.new(x: location.x, y: location.y - 1), direction: location.direction)
+    when 'W'
+      Location.new(coordinates: Coordinates.new(x: (location.x - 1) % 36, y: location.y), direction: location.direction)
+    end
   end
 
   def backwards(location)
