c8502c1a3aa27a49e862596009981db1fed69014
hemalvarambhia <h.n.varambhia@gmail.com>
2023-12-02 21:19:42 +0000

    refactor: duplicated the logic to the new method in the hope that soon it can be removed from Location.

--- lib/non_polar_map.rb
+++ lib/non_polar_map.rb
@@ -13,6 +13,15 @@ class NonPolarMap
   end
 
   def backwards(location)
-    location.backwards
+    case location.direction
+    when 'N'
+      Location.new(coordinates: Coordinates.new(x: location.x, y: location.y - 1), direction: location.direction)
+    when 'E'
+      Location.new(coordinates: Coordinates.new(x: (location.x - 1) % 36, y: location.y), direction: location.direction)
+    when 'S'
+      Location.new(coordinates: Coordinates.new(x: location.x, y: location.y + 1), direction: location.direction)
+    when 'W'
+      Location.new(coordinates: Coordinates.new(x: (location.x + 1) % 36, y: location.y), direction: location.direction)
+    end
   end
 end
