5f4243564e70033aa40bb9bd4259bcefcf3339f8
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-13 19:29:45 +0000

    refactor: removed obsolete code.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -62,23 +62,6 @@ class MarsRover
   end
 
   def map_for(location)
-    if located_at_north_pole?(location)
-      NorthPole.new
-    elsif located_at_south_pole?(location)
-      SouthPole.new
-    else
-      NonPolarMap.new
-    end
     @map.map_for(location)
-  end
-
-  def located_at_south_pole?(location)
-    south_pole_latitude = -9
-    location.y == south_pole_latitude
-  end
-
-  def located_at_north_pole?(location)
-    north_pole_latitude = 9
-    location.y == north_pole_latitude
   end
 end
