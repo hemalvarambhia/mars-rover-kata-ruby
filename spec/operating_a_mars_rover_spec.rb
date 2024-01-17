1352670ab97416d2c6c7bfc86e72a4e27ca1993a
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-17 20:03:07 +0000

    refactor: Mixin the setup module and migrate clients to the creation method.

--- spec/operating_a_mars_rover_spec.rb
+++ spec/operating_a_mars_rover_spec.rb
@@ -1,13 +1,14 @@
 require_relative '../lib/coordinates'
 require_relative '../lib/location'
 require_relative '../lib/mars_rover'
+require 'mars_rover_set_up'
 require_relative './mars_rover_matchers'
 RSpec.describe 'Operating a Mars rover' do
-  include MarsRoverMatchers
+  include MarsRoverMatchers, MarsRoverSetUp
 
   it 'has a starting position at the origin' do
     starting_location = Location.new(direction: 'N', coordinates: Coordinates.new(x: 0, y: 0))
-    mars_rover = MarsRover.new(starting_location: starting_location)
+    mars_rover = mars_rover(starting_at: starting_location)
 
     expected_coordinate = Coordinates.new(x: 0, y: 0)
     expect(mars_rover).to be_located_at(expected_coordinate)
@@ -15,7 +16,7 @@ RSpec.describe 'Operating a Mars rover' do
 
   it 'has a starting position anywhere on the planet' do
     starting_location = Location.new(direction: 'S', coordinates: Coordinates.new(x: 2, y: 3))
-    mars_rover = MarsRover.new(starting_location: starting_location)
+    mars_rover = mars_rover(starting_at: starting_location)
 
     expected_coordinates = Coordinates.new(x: 2, y: 3)
     expect(mars_rover).to be_located_at(expected_coordinates)
@@ -24,7 +25,7 @@ RSpec.describe 'Operating a Mars rover' do
   %w{N E S W}.each do |direction|
     it "can face any direction, including #{direction}" do
       anywhere_facing_direction = Location.new(direction: direction, coordinates: Coordinates.new(x: 2, y: -3))
-      mars_rover = MarsRover.new(starting_location: anywhere_facing_direction)
+      mars_rover = mars_rover(starting_at: anywhere_facing_direction)
 
       expect(mars_rover).to be_facing(direction)
     end
@@ -33,11 +34,11 @@ RSpec.describe 'Operating a Mars rover' do
   it 'cannot be operated when it is facing any direction outside of N, E, S or W' do
     pending('Discussion with customer')
     any_location = Location.new(coordinates: Coordinates.new(x: -2, y: -3), direction: 'Not N, E, S, W')
-    expect { MarsRover.new(starting_location: any_location) }.to raise_error('A Mars rover has to be facing N, E, S or W')
+    expect { mars_rover(starting_at: any_location) }.to raise_error('A Mars rover has to be facing N, E, S or W')
   end
 
   it 'cannot be operated when a starting position is not provided' do
     pending('Discussion with customer')
-    expect { MarsRover.new(starting_location: nil) }.to raise_error('A Mars rover must be given a location to start from')
+    expect { mars_rover(starting_at: nil) }.to raise_error('A Mars rover must be given a location to start from')
   end
 end
