b9bb9684b658ac6b301724fb5643a2cd9b1c7d30
hemalvarambhia <h.n.varambhia@gmail.com>
2024-01-17 12:30:13 +0000

    refactor: added the new set up module.

--- spec/moving_a_mars_rover_forwards_spec.rb
+++ spec/moving_a_mars_rover_forwards_spec.rb
@@ -1,9 +1,10 @@
 require_relative '../lib/coordinates'
 require_relative '../lib/location'
 require_relative '../lib/mars_rover'
+require 'mars_rover_set_up'
 require_relative './mars_rover_matchers'
 describe 'Moving a Mars rover' do
-  include MarsRoverMatchers
+  include MarsRoverMatchers, MarsRoverSetUp
 
   def self.mars_rover(on: Map.new, starting_at:)
     MarsRover.new(map: on, starting_location: starting_at)
