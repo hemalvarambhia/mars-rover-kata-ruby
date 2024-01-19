e4e0215ce4a16729f581d6eb2deea5dff701b66f
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-19 16:55:49 +0000

    refactor: removed obsolete code.

--- spec/mars_rover_set_up.rb
+++ spec/mars_rover_set_up.rb
@@ -2,10 +2,6 @@ require_relative '../lib/map'
 require_relative '../lib/mars_rover'
 require_relative '../lib/obstacle_detector'
 module MarsRoverSetUp
-  def self.mars_rover(on: Map.new, obstacle_detector: ObstacleDetector.new([]), starting_at:)
-    MarsRover.new(map: on, obstacle_detector: obstacle_detector, starting_location: starting_at)
-  end
-
   def a_mars_rover_facing(direction:)
     location = Location.new(direction: direction, coordinates: Coordinates.new(x: 0, y: 0))
     mars_rover(starting_at: location)
