538f42cf9cc50b5cda1590819044c09b7a9c6114
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-19 14:36:07 +0000

    refactor: pushed obstacles detector up towards the tests.

--- spec/mars_rover_set_up.rb
+++ spec/mars_rover_set_up.rb
@@ -3,7 +3,7 @@ require_relative '../lib/mars_rover'
 module MarsRoverSetUp
   private
 
-  def mars_rover(on: Map.with_no_obstacles, starting_at:)
-    MarsRover.new(map: on, starting_location: starting_at)
+  def mars_rover(on: Map.with_no_obstacles, obstacle_detector: ObstacleDetector.new(on.obstacles), starting_at:)
+    MarsRover.new(map: on, obstacle_detector: obstacle_detector, starting_location: starting_at)
   end
 end
