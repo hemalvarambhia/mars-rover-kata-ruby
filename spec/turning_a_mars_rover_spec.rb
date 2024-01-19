9c37e91f14aa43354c830b509dead4f61845ebf0
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-19 14:40:42 +0000

    refactor: pushed a detail up towards the test.

--- spec/turning_a_mars_rover_spec.rb
+++ spec/turning_a_mars_rover_spec.rb
@@ -8,7 +8,7 @@ describe 'Turning a mars rover' do
 
   def self.a_mars_rover_facing(direction:)
     location = Location.new(direction: direction, coordinates: Coordinates.new(x: 0, y: 0))
-    MarsRover.new(starting_location: location, map: Map.with_no_obstacles)
+    MarsRover.new(starting_location: location, map: Map.with_no_obstacles, obstacle_detector: ObstacleDetector.new(Map.with_no_obstacles))
   end
 
   describe 'Turning left' do
