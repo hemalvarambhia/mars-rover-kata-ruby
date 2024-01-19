3afaa39bf24cf7c62f13d7d50ac6056b19e9b091
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-01-19 14:39:10 +0000

    refactor: pushed details up towards the test.

--- spec/moving_a_mars_rover_backwards_spec.rb
+++ spec/moving_a_mars_rover_backwards_spec.rb
@@ -6,8 +6,8 @@ require_relative './mars_rover_matchers'
 describe 'Moving a Mars rover' do
   include MarsRoverMatchers, MarsRoverSetUp
 
-  def self.mars_rover(on: Map.with_no_obstacles, starting_at:)
-    MarsRover.new(map: on, starting_location: starting_at)
+  def self.mars_rover(on: Map.with_no_obstacles, obstacle_detector: ObstacleDetector.new(on.obstacles), starting_at:)
+    MarsRover.new(map: on, obstacle_detector: obstacle_detector, starting_location: starting_at)
   end
 
   describe 'Moving backwards' do
