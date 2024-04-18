6ff3796def7383e100bd98007bab4554f82627f1
Hemal Varambhia <hemal.varambhia@simplybusiness.co.uk>
2024-04-18 15:38:11 +0100

    refactor: we can now use the reflective symmetry of forward and backwards direction to remove duplication.

--- lib/planar_map.rb
+++ lib/planar_map.rb
@@ -16,16 +16,7 @@ class PlanarMap
   end
 
   def self.backwards(location, direction = location.direction)
-    case direction
-    when 'N'
-      Location.new(coordinates: Coordinates.new(x: location.x, y: location.y - 1), direction: location.direction)
-    when 'E'
-      Location.new(coordinates: Coordinates.new(x: (location.x - 1) % 36, y: location.y), direction: location.direction)
-    when 'S'
-      Location.new(coordinates: Coordinates.new(x: location.x, y: location.y + 1), direction: location.direction)
-    when 'W'
-      Location.new(coordinates: Coordinates.new(x: (location.x + 1) % 36, y: location.y), direction: location.direction)
-    end
+    forwards(location, invert(direction))
   end
 
 
