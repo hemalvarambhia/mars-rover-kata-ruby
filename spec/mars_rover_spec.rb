b5404296d2b55dc73c9999a45f5b36b8e453019e
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-06-18 14:31:09 +0100

    feature: a Mars Rover is initially at the origin.

--- spec/mars_rover_spec.rb
+++ spec/mars_rover_spec.rb
@@ -1,4 +1,14 @@
 describe 'Operating a Mars Rover' do
+  class MarsRover
+    def initialize(initial_position)
+
+    end
+
+    def current_position
+      [0, 0]
+    end
+  end
+
   before do
     # before each
   end
@@ -7,7 +17,12 @@ describe 'Operating a Mars Rover' do
     # after each test
   end
 
-  example 'has an initial position'
+  example 'has an initial position' do
+    initial_position = [0, 0]
+    mars_rover = MarsRover.new(initial_position)
+
+    expect(mars_rover.current_position).to eq([0, 0])
+  end
   example 'has a direction it is facing'
 end
 
