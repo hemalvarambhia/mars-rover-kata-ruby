dfadfc7fcf5f24b5bbceb54cb1dd216f7809c26e
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-06-18 14:24:55 +0100

    test: added a test list for the first Mars Rover requirement

---
+++ spec/mars_rover_spec.rb
@@ -0,0 +1,18 @@
+describe 'Operating a Mars Rover' do
+  before do
+    # before each
+  end
+
+  after do
+    # after each test
+  end
+
+  example 'has an initial position'
+  example 'has a direction it is facing'
+end
+
+describe 'Moving a Mars Rover' do
+  example 'moves forward'
+  example 'moves backwards'
+  example 'moves forwards and then backwards'
+end
