117c52ff8c233c76f01a1a719f71e97e75bd3f34
fullStackOasis <fullstackdev@fullstackoasis.com>
2024-07-11 12:52:23 -0400

    refactor: work on making directions more robust.

---
+++ lib/cardinal_directions.rb
@@ -0,0 +1,9 @@
+# A Mars Rover can point in one of four possible directions, definied here.
+module CardinalDirections
+  NORTH = :north
+  SOUTH = :south
+  EAST = :east
+  WEST = :west
+
+  ALL_DIRECTIONS = [NORTH, SOUTH, EAST, WEST].freeze
+end
