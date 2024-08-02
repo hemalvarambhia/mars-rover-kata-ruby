5e870dbc201b0ba1c36af11c05b967da985fcf80
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-08-02 16:37:28 +0100

    refactor: moved the class to the lib directory.

--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -1,15 +1,6 @@
 require 'spec_helper'
 
 describe "Point" do
-  class Point
-    attr_reader :x, :y
-
-    def initialize(x:, y:)
-      @x = x
-      @y = y
-    end
-  end
-
   it 'has an x-coordinate' do
     point = Point.new(x: 0, y: -1)
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@
 require 'logger'
 require_relative '../lib/errors/cannot_command_mars_rover'
 require_relative '../lib/errors/cannot_initialize_mars_rover'
+require_relative '../lib/point'
 require_relative '../lib/mars_rover'
 require_relative '../lib/cardinal_directions'
 
