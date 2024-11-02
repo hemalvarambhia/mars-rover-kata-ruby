8251ba10f4006df4d2052e45142875d020528dfc
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-11-02 19:35:38 +0000

    refactor: migrated code to using the custom built matcher.

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -13,8 +13,7 @@ describe 'Moving a Mars Rover' do
 
       change_only_y_coordinate =
         change(mars_rover, :y_coordinate).by(1).and(change(mars_rover, :x_coordinate).by(0))
-      expect { mars_rover.execute(['f']) }.to change(mars_rover, :y_coordinate).by(1)
-      expect { mars_rover.execute(['f']) }.not_to change(mars_rover, :x_coordinate)
+      expect { mars_rover.execute(['f']) }.to change_only_y_coordinate
     end
 
     it 'moves forwards from any position when facing north' do
