42085bd5d5552cd7cd9e048c55dbfcf591b0a47d
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-21 19:26:36 +0000

    refactor: migrated test to use the custom matcher.

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -20,7 +20,6 @@ describe 'Moving a Mars Rover' do
       initial_position = [0, 2]
       mars_rover = MarsRover.new(initial_position, direction: :north)
 
-      expect { mars_rover.execute(['f']) }.to move(mars_rover, along_y_axis_by: 1)
       expect { mars_rover.execute(['f']) }.to(
         move_forward(mars_rover, displacement: OpenStruct.new(dx: 0, dy: 1))
       )
