78ae80c72a0bd3cb6301eafd467bb0662445ae9c
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-21 19:29:42 +0000

    chore: fixed a deprecation warning.

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -76,8 +76,8 @@ describe 'Moving a Mars Rover' do
     initial_position = [0, 1]
     mars_rover = MarsRover.new(initial_position, direction: :north)
 
-    expect(-> { mars_rover.execute(['b']) }).to change(mars_rover, :y_coordinate).by -1
-    expect(-> { mars_rover.execute(['b']) }).not_to change(mars_rover, :x_coordinate)
+    expect { mars_rover.execute(['b']) }.to change(mars_rover, :y_coordinate).by -1
+    expect { mars_rover.execute(['b']) }.not_to change(mars_rover, :x_coordinate)
   end
 
   it 'moves backwards when facing east' do
