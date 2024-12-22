5a8fbe406d3abce22f28e17355fc794d16eb63fa
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-22 20:58:04 +0000

    refactor: migrated some clients to using Displacement.

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -15,7 +15,7 @@ describe 'Moving a Mars Rover' do
       mars_rover = MarsRover.new(initial_position, direction: :north)
 
       expect { mars_rover.execute(['f']) }.to(
-        move(mars_rover, displacement: OpenStruct.new(dx: 0, dy: 1))
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: 1))
       )
     end
 
@@ -24,7 +24,7 @@ describe 'Moving a Mars Rover' do
       mars_rover = MarsRover.new(initial_position, direction: :north)
 
       expect { mars_rover.execute(['f']) }.to(
-        move(mars_rover, displacement: OpenStruct.new(dx: 0, dy: 1))
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: 1))
       )
     end
 
