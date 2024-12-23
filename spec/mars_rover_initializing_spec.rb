0188cec34fa9d2dc8dc1e8629a011be868641c0c
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-23 22:43:23 +0000

    refactor: migrated away from current_position.

--- spec/mars_rover_initializing_spec.rb
+++ spec/mars_rover_initializing_spec.rb
@@ -6,14 +6,16 @@ describe 'Initializing a Mars Rover' do
     initial_position = [0, 0]
     mars_rover = MarsRover.new(initial_position, direction: :north)
 
-    expect(mars_rover.current_position).to eq([0, 0])
+    expect(mars_rover.x_coordinate).to eq(0)
+    expect(mars_rover.y_coordinate).to eq(0)
   end
 
   example 'has an initial position anywhere on the planet' do
     initial_position = [1, 1]
     mars_rover = MarsRover.new(initial_position, direction: :north)
 
-    expect(mars_rover.current_position).to eq([1, 1])
+    expect(mars_rover.x_coordinate).to eq(1)
+    expect(mars_rover.y_coordinate).to eq(1)
   end
 
   [:north, :south, :east, :west].each do |cardinal_direction|
