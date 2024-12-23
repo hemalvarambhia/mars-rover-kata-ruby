2f2ba623e11355e26cd9fb287033ab794e7b0eda
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-23 20:58:29 +0000

    refactor: no need to change the coordinates twice

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -65,16 +65,16 @@ class MarsRover
     case @direction
     when :north
       @current_position = [@current_position[0], @current_position[1] + 1]
-      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1] + 1)
+      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1])
     when :east
       @current_position = [@current_position[0] + 1, @current_position[1]]
-      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1] + 1)
+      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1])
     when :south
       @current_position = [@current_position[0], @current_position[1] - 1]
-      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1] - 1)
+      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1])
     when :west
       @current_position = [@current_position[0] - 1, @current_position[1]]
-      @current_coordinates = Point.new(x: @current_position[0] - 1, y: @current_position[1])
+      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1])
     end
 
     @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1])
