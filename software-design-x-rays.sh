a80f74d566b5deae6587ddb003d88e57dec6ee9e
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-23 20:53:52 +0000

    chore: default to new year's day 2024.

--- software-design-x-rays.sh
+++ software-design-x-rays.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 function change_frequency() {
-  date="$1"
+  date="${1:-2024-01-01}"
+
   git log --name-only --date=short --format=format: --no-renames --after="$date" | egrep -v '^$' | sort | uniq -c | sort -r
 }
 
