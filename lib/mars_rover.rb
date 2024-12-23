89e8212c9503decf5593247b639aef6a9a8d0974
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-23 20:39:00 +0000

    refactor: in parallel, let's update the current coordinates.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -65,6 +65,7 @@ class MarsRover
     case @direction
     when :north
       @current_position = [@current_position[0], @current_position[1] + 1]
+      @current_coordinates = Point.new(x: @current_position[0], y: @current_position[1] + 1)
     when :east
       @current_position = [@current_position[0] + 1, @current_position[1]]
     when :south
