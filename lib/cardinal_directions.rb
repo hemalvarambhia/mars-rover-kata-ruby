9b75e1cb0d859e9b9742380a30c96c7911ef0eed
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-24 23:42:49 +0000

    refactor: moved the look up table to what feels like a sensible place.

--- lib/cardinal_directions.rb
+++ lib/cardinal_directions.rb
@@ -15,4 +15,13 @@ module CardinalDirections
       west: Displacement.new(dx: -1, dy: 0)
     }[direction]
   end
+
+  def backwards(direction)
+    {
+      north: Displacement.new(dx: 0, dy: -1),
+      east: Displacement.new(dx: -1, dy: 0),
+      south: Displacement.new(dx: 0, dy: 1),
+      west: Displacement.new(dx: 1, dy: 0)
+    }[direction]
+  end
 end
--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -52,13 +52,7 @@ class MarsRover
   end
 
   def move_backwards
-    displacements = {
-      north: Displacement.new(dx: 0, dy: -1),
-      east: Displacement.new(dx: -1, dy: 0),
-      south: Displacement.new(dx: 0, dy: 1),
-      west: Displacement.new(dx: 1, dy: 0)
-    }
-    @current_coordinates += displacements[@direction]
+    @current_coordinates += backwards(@direction)
   end
 
   CARDINAL_COMMANDS = %w[f b l r].freeze
