20b4a2d057d0532c051af9a11e431e1ad5352fb3
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-24 11:08:42 +0000

    refactor: inlined the constants.

--- lib/cardinal_directions.rb
+++ lib/cardinal_directions.rb
@@ -1,9 +1,4 @@
 # A Mars Rover can point in one of four possible directions, definied here.
 module CardinalDirections
-  NORTH = :north
-  SOUTH = :south
-  EAST = :east
-  WEST = :west
-
-  ALL_DIRECTIONS = [NORTH, SOUTH, EAST, WEST].freeze
+  ALL_DIRECTIONS = %i[north south east west].freeze
 end
