f39a7e3afd2b4d881116e046943d708dde566c74
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-24 23:41:23 +0000

    refactor: moved the look up table to what feels like a sensible place.

--- lib/cardinal_directions.rb
+++ lib/cardinal_directions.rb
@@ -6,4 +6,13 @@ module CardinalDirections
   def supported?(direction)
     ALL_DIRECTIONS.include?(direction)
   end
+
+  def forwards(direction)
+    {
+      north: Displacement.new(dx: 0, dy: 1),
+      east: Displacement.new(dx: 1, dy: 0),
+      south: Displacement.new(dx: 0, dy: -1),
+      west: Displacement.new(dx: -1, dy: 0)
+    }[direction]
+  end
 end
--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -48,14 +48,7 @@ class MarsRover
   end
 
   def move_forward
-    displacements = {
-      north: Displacement.new(dx: 0, dy: 1),
-      east: Displacement.new(dx: 1, dy: 0),
-      south: Displacement.new(dx: 0, dy: -1),
-      west: Displacement.new(dx: -1, dy: 0)
-    }
-
-    @current_coordinates += displacements[@direction]
+    @current_coordinates += forwards(@direction)
   end
 
   def move_backwards
