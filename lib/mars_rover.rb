b55bce707daad1933bb165d5117fc74c75dac35a
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-24 11:35:32 +0000

    style: no need for the space.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -70,8 +70,7 @@ class MarsRover
 
   def self.valid_commands?(commands)
     commands.is_a?(Array) &&
-      commands.all? { |command| supported?(command) } &&
-      ! commands.empty?
+      commands.all? { |command| supported?(command) } && !commands.empty?
   end
 
   def self.supported?(command)
