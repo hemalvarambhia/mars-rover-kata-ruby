ac3b9012345dfc1a0229d8e0b5bcc083a61ad828
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-24 21:01:34 +0000

    chore: experiment with maat-scripts git complexity trend idea.

--- software-design-x-rays.sh
+++ software-design-x-rays.sh
@@ -8,3 +8,7 @@ function change_frequency() {
 function complexity() {
   cloc . --by-file --not-match-d='.*\.idea.*'
 }
+
+function complexity_trend() {
+  python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start db1e201083a317bfba6694192c479dc4b55569e1 --end b55bce707daad1933bb165d5117fc74c75dac35a --file lib/mars_rover.rb
+}
