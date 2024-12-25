7aa0676399276c5f3652a754f6edff142396a563
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 19:41:54 +0000

    refactor: moved the moving tests to their own spec.

---
+++ spec/moving_a_mars_rover_spec.rb
@@ -0,0 +1,119 @@
+require 'spec_helper'
+describe 'Moving a Mars Rover' do
+  describe 'Moving forwards' do
+    it 'moves forwards when facing north' do
+      initial_position = [0, 1]
+      mars_rover = MarsRover.new(initial_position, direction: :north)
+
+      expect { mars_rover.execute(['f']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: 1))
+      )
+    end
+
+    it 'moves forwards from any position when facing north' do
+      initial_position = [0, 2]
+      mars_rover = MarsRover.new(initial_position, direction: :north)
+
+      expect { mars_rover.execute(['f']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: 1))
+      )
+    end
+
+    it 'moves forwards from any position when facing south' do
+      initial_position = [-1, -1]
+      mars_rover = MarsRover.new(initial_position, direction: :south)
+
+      expect { mars_rover.execute(['f']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: -1))
+      )
+    end
+
+    it 'moves forwards when facing east' do
+      initial_position = [0, 0]
+      mars_rover = MarsRover.new(initial_position, direction: :east)
+
+      expect { mars_rover.execute(['f']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 1, dy: 0))
+      )
+    end
+
+    it 'moves forwards when facing west' do
+      initial_position = [0, 1]
+      mars_rover = MarsRover.new(initial_position, direction: :west)
+
+      expect { mars_rover.execute(['f']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: -1, dy: 0))
+      )
+    end
+
+    it 'moves forwards multiple times' do
+      mars_rover = MarsRover.new([2, 1], direction: :south)
+
+      expect { mars_rover.execute(%w[f f f]) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: -3))
+      )
+    end
+  end
+
+  describe 'Moving backwards' do
+    it 'moves backwards when facing north' do
+      initial_position = [0, 1]
+      mars_rover = MarsRover.new(initial_position, direction: :north)
+
+      expect { mars_rover.execute(['b']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: -1))
+      )
+    end
+
+    it 'moves backwards from any position when facing north' do
+      initial_position = [2, 1]
+      mars_rover = MarsRover.new(initial_position, direction: :north)
+
+      expect { mars_rover.execute(['b']) }.to(
+        move(mars_rover, displacement: Displacement.new(dx: 0, dy: -1))
+      )
+    end
+
+    it 'moves backwards when facing east' do
+      initial_position = [0, 1]
+      mars_rover = MarsRover.new(initial_position, direction: :east)
+
+      expect { mars_rover.execute(['b']) }.to move(mars_rover, displacement: Displacement.new(dx: -1, dy: 0))
+    end
+
+    it 'moves backwards from any position when facing east' do
+      initial_position = [-1, 1]
+      mars_rover = MarsRover.new(initial_position, direction: :east)
+
+      expect { mars_rover.execute(['b']) }.to move(mars_rover, displacement: Displacement.new(dx: -1, dy: 0))
+    end
+
+    it 'moves backwards when facing south' do
+      mars_rover = MarsRover.new([-1, -1], direction: :south)
+
+      expect { mars_rover.execute(['b']) }.to(move(mars_rover, displacement: Displacement.new(dx: 0, dy: 1)))
+    end
+
+    it 'moves backwards when facing west' do
+      mars_rover = MarsRover.new([3, 1], direction: :west)
+
+      expect { mars_rover.execute(['b']) }.to move(mars_rover, displacement: Displacement.new(dx: 1, dy: 0))
+    end
+  end
+
+  example 'moves forwards and then backwards' do
+    mars_rover = MarsRover.new([0, 1], direction: :east)
+
+    expect { mars_rover.execute(%w[f b b b]) }.to move(mars_rover, displacement: Displacement.new(dx: -2, dy: 0))
+  end
+
+  private
+
+  def move(mars_rover, displacement:)
+    change(mars_rover, :y_coordinate).by(displacement.dy).and(change(mars_rover, :x_coordinate).by(displacement.dx))
+  end
+
+  def not_move(mars_rover)
+    change(mars_rover, :y_coordinate).by(0).and(change(mars_rover, :x_coordinate).by(0))
+  end
+end
