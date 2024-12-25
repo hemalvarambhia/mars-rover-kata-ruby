d38fdc1ce15d0cb22b1d36ddf87924a66e51b1c0
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 19:39:01 +0000

    feat: added the proximity analysis to the script.

--- code-quality-experiments/software-design-x-rays.sh
+++ code-quality-experiments/software-design-x-rays.sh
@@ -15,3 +15,10 @@ function complexity_trend() {
   finishing_commit=$(git log -1 --format="%H" -- "$ruby_script")
   python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file "$ruby_script"
 }
+
+function proximity_analysis() {
+  local ruby_script=$1
+  starting_commit=$(git log --format="%H" --diff-filter=A -- "$ruby_script")
+  finishing_commit=$(git log -1 --format="%H" -- "$ruby_script")
+  python3 ~/projects/maat-scripts/miner/git_proximity_analysis.py --start "$starting_commit" --end "$finishing_commit"
+}
