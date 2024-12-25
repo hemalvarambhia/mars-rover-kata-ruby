85b502bd6135885d63d20045ef287aefdead6179
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 14:40:08 +0000

    refactor: now clients can pass the file to analyse.

--- code-quality-experiments/software-design-x-rays.sh
+++ code-quality-experiments/software-design-x-rays.sh
@@ -10,10 +10,10 @@ function complexity() {
 }
 
 function complexity_trend() {
-  ruby_script=lib/mars_rover.rb
-  starting_commit=$(git log --format="%H" --diff-filter=A -- $ruby_script)
-  finishing_commit=$(git log -1 --format="%H" -- $ruby_script)
-  python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file $ruby_script
+  local ruby_script=$1
+  starting_commit=$(git log --format="%H" --diff-filter=A -- "$ruby_script")
+  finishing_commit=$(git log -1 --format="%H" -- "$ruby_script")
+  python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file "$ruby_script"
 }
 
 function complexity_trend_of_point() {
