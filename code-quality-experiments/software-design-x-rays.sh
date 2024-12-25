408c22783ede8f0bfe931e5dd0bee23ea74a769d
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 14:05:59 +0000

    refactor: we have a general way of getting the first commit for a file.

--- code-quality-experiments/software-design-x-rays.sh
+++ code-quality-experiments/software-design-x-rays.sh
@@ -10,7 +10,7 @@ function complexity() {
 }
 
 function complexity_trend() {
-  starting_commit=940009903d54da750dd25f65ff3375cc52469c9b
+  starting_commit=$(git log --format="%H" --diff-filter=A -- lib/mars_rover.rb)
   finishing_commit=9b75e1cb0d859e9b9742380a30c96c7911ef0eed
   python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end $finishing_commit --file lib/mars_rover.rb
 }
