ccc93b71ee48580498df6734bb97b2d9cf4a7557
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 14:41:57 +0000

    refactor: removed obsolete code.

--- code-quality-experiments/software-design-x-rays.sh
+++ code-quality-experiments/software-design-x-rays.sh
@@ -15,10 +15,3 @@ function complexity_trend() {
   finishing_commit=$(git log -1 --format="%H" -- "$ruby_script")
   python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file "$ruby_script"
 }
-
-function complexity_trend_of_point() {
-  ruby_script=lib/point.rb
-  starting_commit=$(git log --format="%H" --diff-filter=A -- $ruby_script)
-  finishing_commit=$(git log -1 --format="%H" -- $ruby_script)
-  python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file $ruby_script
-}
