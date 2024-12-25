68d30b838188fc3a94bf16e5087b47c3b5ede475
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 14:32:02 +0000

    refactor: removed some duplication in the script path.

--- code-quality-experiments/software-design-x-rays.sh
+++ code-quality-experiments/software-design-x-rays.sh
@@ -17,7 +17,8 @@ function complexity_trend() {
 }
 
 function complexity_trend_of_point() {
-  starting_commit=$(git log --format="%H" --diff-filter=A -- lib/point.rb)
-  finishing_commit=$(git log -1 --format="%H" -- lib/point.rb)
-  python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file lib/point.rb
+  ruby_script=lib/point.rb
+  starting_commit=$(git log --format="%H" --diff-filter=A -- $ruby_script)
+  finishing_commit=$(git log -1 --format="%H" -- $ruby_script)
+  python3 ~/projects/maat-scripts/miner/git_complexity_trend.py --start "$starting_commit" --end "$finishing_commit" --file $ruby_script
 }
