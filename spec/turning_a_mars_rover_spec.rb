5cbb10c60a5aeca6ac1e6a1eb86f37662229b328
Hemal Varambhia <h.n.varambhia@gmail.com>
2024-12-25 14:59:04 +0000

    refactor: moved some tests to their own spec file.

---
+++ spec/turning_a_mars_rover_spec.rb
@@ -0,0 +1,6 @@
+describe 'Turning left' do
+  it 'turns left when facing north, meaning it now faces west'
+  it 'turns left when facing west, meaning it now faces south'
+  it 'turns left when facing south, meaning it now faces east'
+  it 'turns left when facing east, meaning it now faces north'
+end
