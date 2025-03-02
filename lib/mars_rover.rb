53ae5c1eb0eaac366223c4691f479174b7046387
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-03-02 11:59:58 +0000

    refactor: defaulted the initial_position so we can stop having clients pass it in.

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -8,7 +8,7 @@ class MarsRover
 
   attr_reader :direction, :current_coordinates
 
-  def initialize(initial_position, starting_point: Point.new(x: initial_position[0], y: initial_position[1]), direction: :north)
+  def initialize(initial_position = nil, starting_point: Point.new(x: initial_position[0], y: initial_position[1]), direction: :north)
     raise CannotInitializeMarsRover.new unless supported?(direction)
 
     @current_coordinates = starting_point
