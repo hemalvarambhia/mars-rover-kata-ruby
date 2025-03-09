4f46eeedfbd88468bddb68b956b756961877d476
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-03-09 22:36:38 +0000

    refactor: migrated clients to pass in Point.

--- spec/turning_a_mars_rover_spec.rb
+++ spec/turning_a_mars_rover_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 describe 'Turning left' do
   it 'turns left when facing north, meaning it now faces west' do
-    mars_rover = MarsRover.new([0, 0], direction: :north)
+    mars_rover = MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction: :north)
 
     expect { mars_rover.execute(['l']) }.to change(mars_rover, :direction).to :west
   end
   it 'turns left when facing west, meaning it now faces south' do
-    mars_rover = MarsRover.new([0, 0], direction: :west)
+    mars_rover = MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction: :west)
 
     expect { mars_rover.execute(['l']) }.to change(mars_rover, :direction).to(:south)
   end
