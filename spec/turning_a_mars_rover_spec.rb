463424aa46928bf88976ab8cc3a116bec91ad046
Hemal Varambhia <h.n.varambhia@gmail.com>
2025-03-09 23:32:26 +0000

    refactor: extracted duplicate code to a parameterised creation method.

--- spec/turning_a_mars_rover_spec.rb
+++ spec/turning_a_mars_rover_spec.rb
@@ -1,23 +1,29 @@
 require 'spec_helper'
 describe 'Turning left' do
   it 'turns left when facing north, meaning it now faces west' do
-    mars_rover = MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction: :north)
+    mars_rover = mars_rover_facing(direction: :north)
 
     expect { mars_rover.execute(['l']) }.to change(mars_rover, :direction).to :west
   end
   it 'turns left when facing west, meaning it now faces south' do
-    mars_rover = MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction: :west)
+    mars_rover = mars_rover_facing(direction: :west)
 
     expect { mars_rover.execute(['l']) }.to change(mars_rover, :direction).to(:south)
   end
   it 'turns left when facing south, meaning it now faces east' do
-    mars_rover = MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction: :south)
+    mars_rover = mars_rover_facing(direction: :south)
 
     expect { mars_rover.execute(['l']) }.to change(mars_rover, :direction).to(:east)
   end
   it 'turns left when facing east, meaning it now faces north' do
-    mars_rover = MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction: :east)
+    mars_rover = mars_rover_facing(direction: :east)
 
     expect { mars_rover.execute(['l']) }.to change(mars_rover, :direction).to(:north)
+  end
+
+  private
+
+  def mars_rover_facing(direction:)
+    MarsRover.new(starting_point: Point.new(x: 0, y: 0), direction:)
   end
 end
