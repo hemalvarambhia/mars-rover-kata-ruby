6ca5e07f9a1e71301c164f35759d2fc6596b70ca
John Gallagher <john@synapticmishap.co.uk>
2025-04-02 10:21:44 +0100

    Test passes

--- tcr.sh
+++ tcr.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-bundle exec rspec --tag ~skip --format progress && git commit -am "Test passes" || git restore lib
+bundle exec rspec --tag ~skip --format progress && git commit -am "Test passes" || git reset --hard
