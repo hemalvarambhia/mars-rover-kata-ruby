b12e2d26d3eb19769e2846bc83b2826db808fe87
John Gallagher <john@synapticmishap.co.uk>
2025-04-02 10:34:30 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -71,7 +71,8 @@ describe 'Moving a Mars Rover' do
   it 'moves backwards when facing north' do
     initial_position = [0, 1]
     mars_rover = MarsRover.new(initial_position, direction: :north)
-    expect { mars_rover.execute(['b']) }.to change(mars_rover, :current_position).from([0, 1]).to([0, 0])
+    mars_rover.execute(['b'])
+    expect(mars_rover.current_position_object.x).to eq(0)
   end
 
   it 'moves backwards when facing east' do
