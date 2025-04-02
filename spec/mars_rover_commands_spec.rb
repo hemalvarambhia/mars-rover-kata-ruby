e6df3342bdd4fd57fe43721f2133d54390ab5d23
John Gallagher <john@synapticmishap.co.uk>
2025-04-02 10:24:43 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -22,7 +22,7 @@ describe 'Moving a Mars Rover' do
         end
       end
     end
-    initial_position = [0, 1]
+    initial_position = Position.new(x: 0, y: 1)
     mars_rover = MarsRover.new(initial_position, direction: :north)
     mars_rover.execute(['f'])
     expect(mars_rover.current_coordinates.x).to eq(0)
