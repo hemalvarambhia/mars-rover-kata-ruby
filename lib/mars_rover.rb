d206f932d2cb5a9d0ae0ffc522bcc1368462ef35
John Gallagher <john@synapticmishap.co.uk>
2025-04-02 10:41:11 +0100

    Test passes

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -35,10 +35,6 @@ class MarsRover
   end
 
   def current_position
-    current_position_object
-  end
-
-  def current_position_object
     Position.new(x: @current_coordinates.x, y: @current_coordinates.y)
   end
 
