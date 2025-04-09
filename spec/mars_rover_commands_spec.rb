feffcb4657d3646c342eabca5473fa80aa0d66be
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 10:47:06 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -30,7 +30,7 @@ describe 'Moving a Mars Rover' do
   end
 
   it 'does not change its position when there is an unsupported command' do
-    initial_position = [3, -1]
+    initial_position = Position.new(x: 3, y: -1)
     mars_rover = MarsRover.new(initial_position, direction: :north)
     # This tests both the exception raised and that the instance did not change.
     # TODO FIXME we need more validation tests.
