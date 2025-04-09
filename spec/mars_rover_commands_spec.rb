502a616173fc17f7312e010059c2f728f3b68644
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 11:42:31 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -112,7 +112,7 @@ describe 'Moving a Mars Rover' do
     end
 
     failure_message do |mars_rover|
-      "expected the Mars rover to have moved #{@direction} from (0, 0) to #{Position.origin.translate(mars_rover.direction)} but it moved to #{mars_rover.position}"
+      "expected the Mars rover to have moved #{@direction} from #{Position.origin} to #{Position.origin.translate(mars_rover.direction)} but it moved to #{mars_rover.position}"
     end
   end
 
