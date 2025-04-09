644536b22c98a73485b0b589a631f69f799224ec
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 10:47:18 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -44,7 +44,7 @@ describe 'Moving a Mars Rover' do
 
   %i[north south east west].each do |cardinal_direction|
     it "does not change direction from #{cardinal_direction} when there is an unsupported command" do
-      initial_position = [3, -1]
+      initial_position = Position.new(x: 3, y: -1)
       mars_rover = MarsRover.new(initial_position, direction: cardinal_direction)
 
       expect { mars_rover.execute(['q']) }.to raise_error(CannotCommandMarsRover)
