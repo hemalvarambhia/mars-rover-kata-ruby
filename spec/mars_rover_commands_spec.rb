2e97806c41eac23169fd725142775ab3417df226
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 12:04:35 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -1,7 +1,7 @@
 describe 'Moving a Mars Rover' do
   it 'receives an array of commands from Earth' do
     initial_position = Position.new(x: 0, y: 0)
-    mars_rover = MarsRover.new(initial_position, direction: :north)
+    mars_rover = MarsRover.new(initial_position, orientation: :north)
     expect(mars_rover).to respond_to(:execute).with(1)
   end
 
