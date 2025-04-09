9a1ebe68acc7911cddf682dcb923d4300e2e8880
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 11:02:26 +0100

    Test passes

--- lib/mars_rover.rb
+++ lib/mars_rover.rb
@@ -76,8 +76,8 @@ class MarsRover
     when 'b'
       case @direction
       when :north
+        @current_coordinates = @current_coordinates.translate(:south)
         @current_position = [0, @current_position[1] - 1]
-        @current_coordinates = Position.new(x: @current_position[0], y: @current_position[1])
       when :east
         @current_position = [-1, 1]
         @current_coordinates = Position.new(x: @current_position[0], y: @current_position[1])
