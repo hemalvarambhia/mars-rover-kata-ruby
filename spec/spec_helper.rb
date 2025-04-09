6182bfa341fe82a21dbdec7860959011bf5532d0
John Gallagher <johngallagher@biggerpockets.com>
2025-04-09 18:27:04 +0100

    Test passes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require_relative '../lib/errors/cannot_command_mars_rover'
 require_relative '../lib/errors/cannot_initialize_mars_rover'
 require_relative '../lib/point'
 require_relative '../lib/mars_rover'
-require_relative '../lib/cardinal_directions'
 
 RSpec.configure do |config|
   config.before(:suite) do
