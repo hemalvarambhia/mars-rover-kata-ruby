94573b3e6a71aef232e5282e11b683bb16650cba
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 11:56:57 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -64,7 +64,7 @@ describe 'Moving a Mars Rover' do
 
   it 'raises an error if no commands (an empty array) are sent from Earth' do
     initial_position = Position.origin
-    mars_rover = MarsRover.new(initial_position, direction: :north)
+    mars_rover = MarsRover.new(initial_position, orientation: :north)
     expect { mars_rover.execute([]) }.to raise_error(CannotCommandMarsRover)
   end
 
