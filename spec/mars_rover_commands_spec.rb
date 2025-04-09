babe2036ca080c2968ef27478846d4883d64adde
John Gallagher <john@synapticmishap.co.uk>
2025-04-09 12:04:26 +0100

    Test passes

--- spec/mars_rover_commands_spec.rb
+++ spec/mars_rover_commands_spec.rb
@@ -15,7 +15,7 @@ describe 'Moving a Mars Rover' do
 
   it 'moves forwards when facing north from y=2' do
     initial_position = Position.new(x: 0, y: 2)
-    mars_rover = MarsRover.new(initial_position, direction: :north)
+    mars_rover = MarsRover.new(initial_position, orientation: :north)
     mars_rover.execute(['f'])
     expect(mars_rover.position.x).to eq(0)
     expect(mars_rover.position.y).to eq(3)
