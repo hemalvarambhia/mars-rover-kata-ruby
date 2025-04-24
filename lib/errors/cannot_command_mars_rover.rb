2fb5d96f7db8ddc41129f306b1a5fa7923b65aaf
John Gallagher <john@synapticmishap.co.uk>
2025-04-24 11:13:55 +0100

    Test passes

--- lib/errors/cannot_command_mars_rover.rb
+++ lib/errors/cannot_command_mars_rover.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CannotCommandMarsRover < StandardError
-  def initialize(message = 'Cannot command Mars Rover')
+  def initialize(message = 'Cannot command Mars Rover', commands: [])
     super(message)
   end
 end
