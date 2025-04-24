9e63c703b77930f868ff8f4203412d5b0f86c42c
John Gallagher <john@synapticmishap.co.uk>
2025-04-24 11:14:03 +0100

    Test passes

--- lib/errors/cannot_command_mars_rover.rb
+++ lib/errors/cannot_command_mars_rover.rb
@@ -3,5 +3,13 @@
 class CannotCommandMarsRover < StandardError
   def initialize(message = 'Cannot command Mars Rover', commands: [])
     super(message)
+    @commands = commands
+  end
+
+  def to_formatted_h
+    {
+      message:,
+      commands: @commands
+    }
   end
 end
