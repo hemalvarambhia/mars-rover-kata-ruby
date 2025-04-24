7f66f4b78ec44bc0ac1c7d4202c3b63f05b96e72
John Gallagher <john@synapticmishap.co.uk>
2025-04-24 11:16:36 +0100

    Test passes

--- lib/errors/cannot_command_mars_rover.rb
+++ lib/errors/cannot_command_mars_rover.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# Error raised when trying to command a Mars Rover with invalid commands
+#
+# @example Raise error with default message
+#   raise CannotCommandMarsRover.new
+#
+# @example Raise error with commands
+#   commands = ['f']
+#   raise CannotCommandMarsRover.new(commands: commands)
+#
+# @example Create error from commands
+#   commands = ['f']
+#   CannotCommandMarsRover.from(commands: commands)
 class CannotCommandMarsRover < StandardError
   def initialize(message = 'Cannot command Mars Rover', commands: [])
     super(message)
@@ -10,6 +22,9 @@ class CannotCommandMarsRover < StandardError
     new(commands:)
   end
 
+  # Returns a hash representation of the error
+  #
+  # @return [Hash] Hash containing error message and commands
   def to_formatted_h
     {
       message:,
--- lib/errors/too_many_commands.rb
+++ lib/errors/too_many_commands.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# Error raised when too many commands are sent to a Mars Rover
+#
+# @example Raise error with default message
+#   raise TooManyCommands.new
+#
+# @example Raise error with commands
+#   commands = ['f'] * 11
+#   raise TooManyCommands.new(commands: commands)
+#
+# @example Create error from commands
+#   commands = ['f'] * 11
+#   TooManyCommands.from(commands: commands)
 class TooManyCommands < StandardError
   def initialize(message = 'Too many commands', commands: [])
     super(message)
@@ -10,6 +22,9 @@ class TooManyCommands < StandardError
     new(commands:)
   end
 
+  # Returns a hash representation of the error
+  #
+  # @return [Hash] Hash containing error message and commands
   def to_formatted_h
     {
       message:,
