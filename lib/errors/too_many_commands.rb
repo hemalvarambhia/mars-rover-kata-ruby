59dfc11c4787a3baf126c9c856aecf60b2bdf909
John Gallagher <john@synapticmishap.co.uk>
2025-04-24 11:00:18 +0100

    Test passes

--- lib/errors/too_many_commands.rb
+++ lib/errors/too_many_commands.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class TooManyCommands < StandardError
-  def initialize(message = 'Too many commands')
-    super
+  def initialize(message = 'Too many commands', commands: [])
+    super(message)
   end
 
   def to_formatted_h
